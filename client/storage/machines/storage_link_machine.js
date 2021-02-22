dc5b182f2c31be2ece1f7645e33a2785c4c4ebb6
Darren Jensen <darren.jensen@gmail.com>
2021-02-22 12:16:34 +0700

    move long state machine transition functions

--- client/storage/machines/storage_link_machine.js
+++ client/storage/machines/storage_link_machine.js
@@ -1,7 +1,58 @@
-// State Machine Definitions
 const { Machine } = require('xstate');
 
-exports.createStateMachine = function createStateMachine(model, chunk, storage) {
+exports.createStateMachine = function createStateMachine(link, chunk, storage) {
+  async function prepareChunkSegment() {
+    await link.refresh()
+    const key = await link.getRedkey();
+    return [
+      link.merkle_root,
+      link.segment_hashes.map(x=>Buffer.from(x, 'hex')),
+      link.encrypted_length,
+      chunk.id,
+      key.pub,
+      chunk.length,
+    ];
+  }
+
+  async function prepareChunkData() {
+    if (!link.segments_sent) link.segments_sent = {};
+    if (!link.segments_received) link.segments_received = [];
+
+    const totalSegments = link.segment_hashes.length;
+    let idx = -1;
+    for(let i = 0; i < totalSegments; i++) {
+        if (!link.segments_sent[i]) {
+            idx = i; break;
+        }
+        // Retransmit timed out segments
+        // todo: give up after X attempts to retransmit
+        if (!storage.isProviderQueueFull(link.provider_id)) {
+            if (!link.segments_received.includes(i) && link.segments_sent[i] && link.segments_sent[i] + storage.config.retransmit_segments_timeout_seconds < Math.floor(Date.now()/1000)) {
+                console.log('retransmitting chunk', link.chunk_id, 'segment', i);
+                idx = i;
+                break
+            }
+        }
+    }
+    if (idx === -1) {
+        return
+    }
+
+    link.segments_sent[idx] = Math.floor(Date.now()/1000);
+    await link.save();
+
+    const encryptedData = link.getEncryptedData(); // todo: optimize using fs ranges // todo: use fs async
+
+    const SEGMENT_SIZE_BYTES = storage.ctx.config.storage.segment_size_bytes;
+
+    return [
+        link.merkle_root,
+        idx,
+        // Note: Buffer.slice is (start, end) not (start, length)
+        encryptedData.slice(idx * SEGMENT_SIZE_BYTES, idx * SEGMENT_SIZE_BYTES + SEGMENT_SIZE_BYTES),
+    ];
+  }
+
   return Machine(
     {
       id: 'storageLink',
@@ -17,8 +68,8 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'SEND_STORE_CHUNK_REQUEST',
             src: async (context, event) => {
-              await model.save()
-              return storage.SEND_STORE_CHUNK_REQUEST(chunk, model)
+              await link.save()
+              return storage.SEND_STORE_CHUNK_REQUEST(chunk, link)
             },
             onDone: {
               target: 'encrypting',
@@ -34,8 +85,8 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'ENCRYPT_CHUNK',
             src: async (context, event) => {
-              await model.save()
-              return storage.ENCRYPT_CHUNK(chunk, model)
+              await link.save()
+              return storage.ENCRYPT_CHUNK(chunk, link)
             },
             onDone: {
               target: 'sending_segment_map'
@@ -50,17 +101,9 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'SEND_STORE_CHUNK_SEGMENTS',
             src: async (context, event) => {
-              await model.refresh()
-              const key = await model.getRedkey();
-              const data = [
-                model.merkle_root,
-                model.segment_hashes.map(x=>Buffer.from(x, 'hex')),
-                model.encrypted_length,
-                chunk.id,
-                key.pub,
-                chunk.length,
-              ];
-              return storage.SEND_STORE_CHUNK_SEGMENTS(data, model)
+              await link.save()
+              const data = await prepareChunkSegment()
+              return storage.SEND_STORE_CHUNK_SEGMENTS(data, link)
             },
             onDone: {
               target: 'sending_data',
@@ -81,44 +124,9 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'SEND_STORE_CHUNK_DATA',
             src: async (context, event) => {
-              if (!model.segments_sent) model.segments_sent = {};
-              if (!model.segments_received) model.segments_received = [];
-
-              const totalSegments = model.segment_hashes.length;
-              let idx = -1;
-              for(let i = 0; i < totalSegments; i++) {
-                  if (!model.segments_sent[i]) {
-                      idx = i; break;
-                  }
-                  // Retransmit timed out segments
-                  // todo: give up after X attempts to retransmit
-                  if (!storage.isProviderQueueFull(model.provider_id)) {
-                      if (!model.segments_received.includes(i) && model.segments_sent[i] && model.segments_sent[i] + storage.config.retransmit_segments_timeout_seconds < Math.floor(Date.now()/1000)) {
-                          console.log('retransmitting chunk', model.chunk_id, 'segment', i);
-                          idx = i;
-                          break
-                      }
-                  }
-              }
-              if (idx === -1) {
-                  return
-              }
-
-              model.segments_sent[idx] = Math.floor(Date.now()/1000);
-              await model.save();
-
-              const encryptedData = model.getEncryptedData(); // todo: optimize using fs ranges // todo: use fs async
-
-              const SEGMENT_SIZE_BYTES = storage.ctx.config.storage.segment_size_bytes;
-
-              const data = [
-                  model.merkle_root,
-                  idx,
-                  // Note: Buffer.slice is (start, end) not (start, length)
-                  encryptedData.slice(idx * SEGMENT_SIZE_BYTES, idx * SEGMENT_SIZE_BYTES + SEGMENT_SIZE_BYTES),
-              ];
-              // await model.save()
-              return storage.SEND_STORE_CHUNK_DATA(data, model)
+              await link.save()
+              const data = await prepareChunkData()
+              return storage.SEND_STORE_CHUNK_DATA(data, link)
             },
             onDone: {
               target: 'asking_for_signature',
@@ -134,8 +142,8 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'SEND_STORE_CHUNK_SIGNATURE_REQUEST',
             src: async (context, event) => {
-              await model.save()
-              return storage.SEND_STORE_CHUNK_SIGNATURE_REQUEST(model)
+              await link.save()
+              return storage.SEND_STORE_CHUNK_SIGNATURE_REQUEST(link)
             },
             onDone: {
               target: 'signed',
@@ -151,7 +159,7 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
           invoke: {
             id: 'SAVE_MODEL_SIGNED',
             src: async () => {
-              await model.save()
+              await link.save()
               storage.uploadingChunksProcessing[chunk.id] = false;
               return chunk.reconsiderUploadingStatus(true);
             }
@@ -166,10 +174,10 @@ exports.createStateMachine = function createStateMachine(model, chunk, storage) 
     {
       actions: {
         UPDATE_LEGACY_STATUS: async () => {
-          model.status = model.state
+          link.status = link.state
         },
         UPDATE_MODEL_ERR: async (context, event) => {
-          model.err = event.data
+          link.err = event.data
         },
       },
       guards: {
--- db/models/storage_link.js
+++ db/models/storage_link.js
@@ -11,14 +11,14 @@ class StorageLink extends Model {
       super(...args);
     }
 
-    initStateMachine(chunk=undefined, initialState='initialized') {
+    initStateMachine(chunk) {
       // create a state machine using the factory
       this._stateMachine = storageLinkMachine.createStateMachine(this, chunk, this.ctx.client.storage)
 
       this._storageLinkService = interpret(this._stateMachine)//.onTransition(state => console.log(`Current State: ${state.value}`))
 
       // start the storage link machine service
-      this._storageLinkService.start(initialState)
+      this._storageLinkService.start()
     }
 
     get machine() {
