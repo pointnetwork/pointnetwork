8434ea6e38c78b6d253ea5870f998fe696ccd381
Darren Jensen <darren.jensen@gmail.com>
2021-03-09 16:27:36 +0700

    add static index route and script import to index html

--- api/api_routes.js
+++ api/api_routes.js
@@ -1,6 +1,7 @@
 module.exports = [
     ['GET', '/', 'WebController@index'],
     ['GET', '/static/:file', 'WebController@static'],
+    ['GET', '/static/', 'WebController@index'],
 
     ['GET', '/ping', 'PingController@ping'],
     ['GET', '/deploy', 'DeployController@deploy'],
