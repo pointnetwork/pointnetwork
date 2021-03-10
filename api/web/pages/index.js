5ebeb2b03601bd7959280c6a8e1e0019e0265cce
Darren Jensen <darren.jensen@gmail.com>
2021-03-10 14:52:16 +0700

    minor modifications to layout and css

--- api/web/pages/index.js
+++ api/web/pages/index.js
@@ -1,37 +1,37 @@
 import Layout from '../components/layout'
 
 export default () => (
-  <Layout>
-    <div>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">Point Network Web API</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Point Network Web API</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <div class="input-group mb-1">
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2"></input>
+            <div class="input-group-append">
+              <button class="btn btn-outline-success" type="button">Search</button>
+            </div>
           </div>
-      </nav>
-      <main role="main" class="container">
-
-        <div class="starter-template">
+        </form>
+      </div>
+    </nav>
+    <Layout>
+        <div>
             <h1>Welcome to Pointnetwork!</h1>
             <p class="lead">Web app utility for Pointnetwork Nodes.</p>
             <h2>Deployer Progress</h2>
-            <div id="app"></div>
+            {/* TODO Deployer Progress Component! */}
         </div>
-
-      </main>
-    </div>
-  </Layout>
+    </Layout>
+  </div>
 )
 
