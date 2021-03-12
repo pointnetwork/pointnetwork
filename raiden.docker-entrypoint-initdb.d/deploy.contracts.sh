b3b2cfeecde1e9c756b0f71dc7952da81e7b320d
den <d.n.voropaev@gmail.com>
2021-03-12 10:34:51 +0300

    added miner keystore, stored deployed secret and token network registry contract addresses in files

--- raiden.docker-entrypoint-initdb.d/deploy.contracts.sh
+++ raiden.docker-entrypoint-initdb.d/deploy.contracts.sh
@@ -6,7 +6,7 @@ source /opt/venv/bin/activate
 
 # export VERSION=$(grep 'CONTRACTS_VERSION = ' -r /opt/venv/lib/python3.7/site-packages/raiden_contracts | awk '{ print $3 }')
 export VERSION="0.25.0"
-export PRIV_KEY="/keystore/$(ls /keystore/)"
+export PRIV_KEY="/keystore/$(ls /keystore/ | grep c01011611e3501c6b3f6dc4b6d3fe644d21ab301)"
 export KEY_PSWD="/pswrd"
 export MAX_UINT256=115792089237316195423570985008687907853269984665640564039457584007913129639935
 export PROVIDER="http://geth:8545"
@@ -16,11 +16,23 @@ echo "Private key: $PRIV_KEY"
 echo "Max UInt256: $MAX_UINT256"
 echo "Provider: $PROVIDER"
 
-# export RESULT=$(printf "\n" | python -m raiden_contracts.deploy raiden --rpc-provider $PROVIDER --private-key $PRIV_KEY --gas-price 10 --gas-limit 6000000 --contracts-version $VERSION --max-token-networks $MAX_UINT256)
-python -m raiden_contracts.deploy raiden --rpc-provider $PROVIDER --private-key $PRIV_KEY --gas-price 10 --gas-limit 6000000 --contracts-version $VERSION --max-token-networks $MAX_UINT256 << EOF
+if [[ -f tmp/SecretRegistry && -f tmp/TokenNetworkRegistry ]]; then
+    echo "Restoring contracts from files"
 
-EOF
+    export SecretRegistry=$(cat /tmp/SecretRegistry)
+    export TokenNetworkRegistry=$(cat /tmp/TokenNetworkRegistry)
+else
+    echo "Deploying contracts"
 
-# echo "Result: $RESULT"
+    export RESULT=$(printf "\n" | python -m raiden_contracts.deploy raiden --rpc-provider "$PROVIDER" --private-key "$PRIV_KEY" --gas-price 10 --gas-limit 6000000 --contracts-version "$VERSION" --max-token-networks "$MAX_UINT256")
+    export SecretRegistry=$(echo "$RESULT" | grep -E "^(\{|\}|\s{4,})" | python -c "import sys, json; print(json.load(sys.stdin)['SecretRegistry'])")
+    export TokenNetworkRegistry=$(echo "$RESULT" | grep -E "^(\{|\}|\s{4,})" | python -c "import sys, json; print(json.load(sys.stdin)['TokenNetworkRegistry'])")
+
+    echo "$SecretRegistry" > /tmp/SecretRegistry
+    echo "$TokenNetworkRegistry" > /tmp/TokenNetworkRegistry
+fi
+
+echo "Captured SecretRegistry: $SecretRegistry"
+echo "Captured TokenNetworkRegistry: $TokenNetworkRegistry"
 
 echo "Done so far"
