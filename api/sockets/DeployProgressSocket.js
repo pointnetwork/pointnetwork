dc79706b72aa9cfb515e55dbd4675b924b4d357a
Darren Jensen <darren.jensen@gmail.com>
2021-03-16 15:39:31 +0700

    move common socket logic to base class and wire up ws commands to controllers

--- api/sockets/DeployProgressSocket.js
+++ api/sockets/DeployProgressSocket.js
@@ -1,34 +1,9 @@
-const WebSocket = require('ws');
-const Console = require('../../console');
+const Socket = require('./Socket')
 
-class DeployProgressSocket {
+class DeployProgressSocket extends Socket {
   constructor(ctx, conn, wss) {
-    this.ctx = ctx;
-    this.ws = conn.socket;
-    this.wss = wss;
+    super(ctx, conn, wss)
     this.ctx.client.deployerProgress.wss = this
-    this.init()
-  }
-
-  init() {
-    this.console = new Console();
-    this.ws.on('message', async (message) => {
-      this.publishToClients(await this.responseFor(message))
-    })
-  }
-
-  publishToClients(progress) {
-    if(this.wss) {
-      this.wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(progress));
-        }
-      });
-    }
-  }
-
-  async responseFor(message) {
-    return await this.console.cmd_api(`api/${message}`)
   }
 }
 
