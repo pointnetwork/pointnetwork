9667d1c314e15b8a27c17f05a6657f7986b89e21
Darren Jensen <darren.jensen@gmail.com>
2021-03-19 11:04:02 +0700

    added new api node status controller and endpoint

--- api/api_routes.js
+++ api/api_routes.js
@@ -1,5 +1,6 @@
 module.exports = [
     ['GET', '/api/ping', 'PingController@ping'],
+    ['GET', '/api/status', 'StatusController@status'],
     ['GET', '/api/deploy', 'DeployController@deploy'],
     ['GET', '/api/storage/files', 'StorageController@files'],
     ['GET', '/api/storage/files/:id', 'StorageController@fileById'],
