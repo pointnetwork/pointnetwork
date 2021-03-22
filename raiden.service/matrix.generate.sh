708614abd108eee6922930ad282dffb9f236fbf5
den <d.n.voropaev@gmail.com>
2021-03-22 23:55:56 +0300

    added matrix setup and generator script

---
+++ raiden.service/matrix.generate.sh
@@ -0,0 +1,7 @@
+docker run -it --rm \
+    -v $(pwd)/matrix_data:/data \
+    -e SYNAPSE_SERVER_NAME=matrix \
+    -e SYNAPSE_REPORT_STATS=yes \
+    -e UID=1000 \
+    -e GID=1000 \
+    matrixdotorg/synapse:v1.30.0rc1 generate
