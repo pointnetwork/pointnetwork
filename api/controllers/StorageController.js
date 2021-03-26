44eb35af1c1a3a7522353ba2514d96d5e72381b2
Darren Jensen <darren.jensen@gmail.com>
2021-03-26 16:23:42 +0700

    added downloaded files / chunks to api endpoint and use UDP transport in Kademlia

--- api/controllers/StorageController.js
+++ api/controllers/StorageController.js
@@ -8,7 +8,10 @@ class StorageController {
   }
 
   async files() {
-    const allFiles = await File.allBy('ul_status', File.UPLOADING_STATUS_UPLOADED)
+    const allUploadedFiles = await File.allBy('ul_status', File.UPLOADING_STATUS_UPLOADED)
+    const allDownloadedFiles = await File.allBy('dl_status', File.DOWNLOADING_STATUS_DOWNLOADED)
+    // union all uploaded and downloaded files to a unique list
+    var allFiles = [...new Set([...allUploadedFiles, ...allDownloadedFiles])];
     // return a subset of the attributes to the client
     const files = allFiles.map((file) =>
       (({ id, localPath, size, redundancy, expires, autorenew, chunkIds }) => ({ id, localPath, size, redundancy, expires, autorenew, chunkCount: chunkIds.length }))(file))
@@ -23,7 +26,10 @@ class StorageController {
   }
 
   async chunks() {
-    const allChunks = await Chunk.allBy('ul_status', Chunk.UPLOADING_STATUS_UPLOADED)
+    const allUploadedChunks = await Chunk.allBy('ul_status', Chunk.UPLOADING_STATUS_UPLOADED)
+    const allDownloadedChunks = await Chunk.allBy('dl_status', Chunk.DOWNLOADING_STATUS_DOWNLOADED)
+    // union all uploaded and downloaded chunks to a unique list
+    var allChunks = [...new Set([...allUploadedChunks, ...allDownloadedChunks])];
     // return a subset of the attributes to the client
     const chunks = allChunks.map((chunk) =>
       (({ id, redundancy, expires, autorenew }) => ({ id, redundancy, expires, autorenew }))(chunk))
--- scripts/db/playground.js
+++ scripts/db/playground.js
@@ -10,20 +10,33 @@ require('./init')
 // now use the db models to check the leveldb database
 const File = require('../../db/models/file');
 const Chunk = require('../../db/models/chunk');
+const ProviderChunk = require('../../db/models/provider_chunk');
 
 (async() => {
   // Some examples below to try:
-  const allFiles = await File.allBy('ul_status', File.UPLOADING_STATUS_UPLOADED)
+  const allUploadedFiles = await File.allBy('ul_status', File.UPLOADING_STATUS_UPLOADED)
+  const allDownloadedFiles = await File.allBy('dl_status', File.DOWNLOADING_STATUS_DOWNLOADED)
+
+  var allFiles = [...new Set([...allUploadedFiles, ...allDownloadedFiles])];
 
   const files = allFiles.map((file) =>
     (({ id, localPath, size, redundancy, expires, autorenew, chunkIds }) => ({ id, localPath, size, redundancy, expires, autorenew, chunkCount: chunkIds.length }))(file))
 
   console.log(files)
 
-  const allChunks = await Chunk.allBy('ul_status', Chunk.UPLOADING_STATUS_UPLOADED)
+  // const allChunks = await Chunk.allBy('ul_status', Chunk.UPLOADING_STATUS_UPLOADED)
+  const allUploadedChunks = await Chunk.allBy('ul_status', Chunk.UPLOADING_STATUS_UPLOADED)
+  const allDownloadedChunks = await Chunk.allBy('dl_status', Chunk.DOWNLOADING_STATUS_DOWNLOADED)
+  // union all uploaded and downloaded chunks to a unique list
+  var allChunks = [...new Set([...allUploadedChunks, ...allDownloadedChunks])];
   chunks = allChunks.map((chunk) => chunk._attributes)
+
   console.log(chunks)
 
   const fileKeys = await File.allKeys();
   console.log(`FileCount: ${fileKeys.length}`);
+
+  const allProviderChunks = await ProviderChunk.allBy('status', ProviderChunk.STATUS_CREATED)
+  pchunks = allProviderChunks.map((chunk) => chunk._attributes)
+  console.log(pchunks)
 })()
