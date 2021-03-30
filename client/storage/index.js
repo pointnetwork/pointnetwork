d36bff9e3980c6cf76b9793db49ebc3050415b8a
tunji17 <tunjiabioye@outlook.com>
2021-03-30 17:47:46 +0100

    conditionally turns raiden off and on

--- client/storage/index.js
+++ client/storage/index.js
@@ -13,6 +13,7 @@ const {
     createChannel,
     makePayment,
   } = require('./payments')
+const defaultConfig = require('../../resources/defaultConfig.json');
 
 class Storage {
     constructor(ctx) {
@@ -398,20 +399,18 @@ class Storage {
                         signature
                     };
                     link.validatePledge();
-
-                    // Uncomment after demo for payments
-
-                    // const provider = await link.provider
-                    // const checksumAddress = await this.ctx.web3bridge.toChecksumAddress(`0x${provider.id.split('#')[1]}`)
-                    // await makePayment(checksumAddress, 10) // todo: calculate amount using cost per kb for service provider
-
+                    if (defaultConfig.raiden.switch === 'ON') {
+                        const provider = await link.provider
+                        const checksumAddress = await this.ctx.web3bridge.toChecksumAddress(`0x${provider.id.split('#')[1]}`)
+                        await makePayment(checksumAddress, 10) // todo: calculate amount using cost per kb for service provider
+                    }
                     // const chunk = await link.getChunk();
                     // await chunk.reconsiderUploadingStatus(true); <-- already being done after this function is over, if all is good, remove this block
 
                     resolve(true);
                 } catch (e) {
                     // todo: don't just put this into the console, this is for debugging purposes
-                    console.debug('FAILED CHUNK', {err, result}, chunk.id, e);
+                    console.debug('FAILED CHUNK', {err, result}, e);
                     reject(e);
                 }
             });
