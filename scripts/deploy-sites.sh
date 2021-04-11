6873693b223688006eb99b7299136161607bef27
den <d.n.voropaev@gmail.com>
2021-04-11 23:20:25 +0300

    added website owner service, made `deploy-sites.sh` script usable in a linux alpine container

--- scripts/deploy-sites.sh
+++ scripts/deploy-sites.sh
@@ -1,21 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
 # NOTE: You need to start up Point Network before running this!
 # Run this script from within the project root folder like so:
 # ./scripts/deploy-sites.sh
 
-EXAMPLE_SITES=(./example/*/)
+EXAMPLE_SITES="./example/*"
+if [ $# -eq 0 ]; then
+  ARGS="--datadir ~/.point/test2"
+else
+  ARGS="$@"
+fi
 
-for SITE in ${EXAMPLE_SITES[@]};
+for SITE in $EXAMPLE_SITES;
 do
   echo
   echo "DEPLOYING: ${SITE}"
   echo
 
-  ./point deploy $SITE --datadir ~/.point/test2
+  ./point deploy $SITE $ARGS
   sleep 10
 
   echo
   echo "FINISHED: ${SITE}"
   echo
 done
+
+if [ $# -eq 0 ]; then
+  exit
+fi
+
+./point $ARGS
