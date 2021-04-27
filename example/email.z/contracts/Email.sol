5da7d1fd2f141a83d0346557110b4bf51b55191b
tunji17 <tunjiabioye@outlook.com>
2021-04-27 08:16:28 +0100

    adds email.sol boiler plate

--- example/email.z/contracts/Email.sol
+++ example/email.z/contracts/Email.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-contract Email  {
-   event SendEmail(address indexed to,
-                   string message);
+contract Email {
+  struct mail {
+    address from;
+    address to;
+    string encryptedMessageHash;
+    string encryptedSymmetricKey;
+    uint timestamp;
+  }
+  mail[] mails;
+  event SendEmail(address from, address indexed to, string encryptedMessageHash, string encryptedSymmetricKey, uint timestamp);
 
-   function send(
-     address to,
-     string memory message
-    ) public returns (bool result) {
-       emit SendEmail(to, message);
-
-       return true;
-   }
+  function send(address memory to, string memory encryptedMessageHash, string memory encryptedSymmetricKey) public  {
+        mail memory _mail = mail(msg.sender, to, encryptedMessageHash, encryptedSymmetricKey, block.timestamp);
+        mails.push(_mail);
+        emit SendEmail(msg.sender, to, encryptedMessageHash, encryptedSymmetricKey, timestamp);
+        return true;
+  }
 }
