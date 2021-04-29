4ead09430be42eba8dab3fb7302edf405b07d0d9
tunji17 <tunjiabioye@outlook.com>
2021-04-29 12:52:58 +0100

    accepts data from the post email form encrypts message tries to encrypt symetrickey used in encrypting message

--- example/email.z/contracts/Email.sol
+++ example/email.z/contracts/Email.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-// contract Email {
-//   struct mail {
-//     address from;
-//     address to;
-//     string encryptedMessageHash;
-//     string encryptedSymmetricKey;
-//     uint timestamp;
-//   }
-//   mail[] mails;
-//   event SendEmail(address from, address indexed to, string encryptedMessageHash, string encryptedSymmetricKey, uint timestamp);
+contract Email {
+  struct mail {
+    address from;
+    address to;
+    string encryptedMessageHash;
+    string encryptedSymmetricKey;
+    uint timestamp;
+  }
+  mail[] mails;
+  event SendEmail(address from, address indexed to, string encryptedMessageHash, string encryptedSymmetricKey, uint timestamp);
 
   function send(address to, string memory encryptedMessageHash, string memory encryptedSymmetricKey) public returns (bool result)  {
         mail memory _mail = mail(msg.sender, to, encryptedMessageHash, encryptedSymmetricKey, block.timestamp);
@@ -19,5 +19,4 @@ pragma experimental ABIEncoderV2;
         emit SendEmail(msg.sender, to, encryptedMessageHash, encryptedSymmetricKey, block.timestamp);
         return true;
   }
-
 }
