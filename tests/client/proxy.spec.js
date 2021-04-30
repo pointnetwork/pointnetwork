85bdf1b94618c2b387f8ea3beda2a4428fee4789
den <d.n.voropaev@gmail.com>
2021-04-30 23:14:08 +0300

    added a ecies working example

--- tests/client/proxy.spec.js
+++ tests/client/proxy.spec.js
@@ -36,4 +36,18 @@ describe("Client/ZProxy", () => {
             expect(zproxy.sanitize(input)).toEqual(input);
         });
     });
+
+    test('encrypts a plain text with a random symmetric key and the key itself with ecies', async () => {
+        try {
+            const plaintext = 'Foo'
+            const publicKey = '0x1b26e2c556ae71c60dad094aa839162117b28a462fc4c940f9d12675d3ddfff2aeef60444a96a46abf3ca0a420ef31bff9f4a0ddefe1f80b0c133b85674fff34'
+
+            const result = await ZProxy.encryptPlainTextAndKey(plaintext, publicKey)
+
+            console.log({result})
+        } catch (e) {
+            console.error(e)
+            throw e
+        }
+    })
 });
