520a4447e492ffabd25e2b60195068051a8c0503
den <d.n.voropaev@gmail.com>
2021-05-09 23:23:08 +0300

    updated layout

--- example/store.z/src/src/index.tsx
+++ example/store.z/src/src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { App } from './App'
 
-import './reset.css'
+// import './reset.css'
 
 ReactDom.render(<App />, document.getElementById('root'))
