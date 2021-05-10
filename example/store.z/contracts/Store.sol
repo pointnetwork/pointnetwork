9957a0d734ccafecc0c1943803045f0df4946d89
Darren Jensen <darren.jensen@gmail.com>
2021-05-10 18:46:38 +0700

    added stores seed data to point.deploy.json and example contract call to fetch all stores

--- example/store.z/contracts/Store.sol
+++ example/store.z/contracts/Store.sol
@@ -2,4 +2,23 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-contract Store {}
+contract Store {
+
+  struct StoreFront {
+    string id;
+    string name;
+    string description;
+    string logo;
+  }
+
+  StoreFront[] stores;
+
+  function registerStore(string memory storeId, string memory name, string memory description, string memory logo) public {
+    StoreFront memory _store = StoreFront(storeId, name, description, logo);
+    stores.push(_store);
+  }
+
+  function getStores() public view returns(StoreFront[] memory) {
+    return stores;
+  }
+}
