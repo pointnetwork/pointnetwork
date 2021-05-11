2c95165a88b819de14fa55a26aa5c9312f9dcdee
Darren Jensen <darren.jensen@gmail.com>
2021-05-11 16:07:09 +0700

    added basic products impl to store for data seed example

--- example/store.z/contracts/Store.sol
+++ example/store.z/contracts/Store.sol
@@ -11,7 +11,16 @@ contract Store {
     string logo;
   }
 
+  struct Product {
+    string id;
+    string name;
+    string description;
+    uint price;
+  }
+
   StoreFront[] stores;
+
+  mapping(string => Product[]) productsByStoreId;
 
   function registerStore(string memory storeId, string memory name, string memory description, string memory logo) public {
     StoreFront memory _store = StoreFront(storeId, name, description, logo);
@@ -20,5 +29,16 @@ contract Store {
 
   function getStores() public view returns(StoreFront[] memory) {
     return stores;
+  }
+
+  function getProductsByStoreId(string memory storeId) public view  returns(Product[] memory) {
+    return productsByStoreId[storeId];
+  }
+
+  function addProductToStore(string memory storeId, string memory productId, string memory name, string memory description, uint price) public {
+    // create new product
+    Product memory _product = Product(productId, name, description, price);
+    // add to the productsByStoreId mapping
+    productsByStoreId[storeId].push(_product);
   }
 }
