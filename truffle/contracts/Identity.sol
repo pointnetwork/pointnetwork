51a33dd073f08394b960448c4a4d8b399ed100ad
Darren Jensen <darren.jensen@gmail.com>
2021-05-18 17:20:26 +0700

    added Blog Smart Contract to blog.z example

--- truffle/contracts/Identity.sol
+++ truffle/contracts/Identity.sol
@@ -107,7 +107,8 @@ contract Identity {
     }
 
     modifier onlyIdentityOwner(string memory identity) {
-        if (msg.sender != identityToOwner[identity]) revert('You are not the owner of this identity'); // todo: identityToOwner[identity] == address(0) ?
+        require(msg.sender == identityToOwner[identity], 'You are not the owner of this identity');
+        // todo: identityToOwner[identity] == address(0) ?
         _;
     }
 
