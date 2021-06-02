405f5f0f730696970051caf9d882e5c95338bfa0
Darren Jensen <darren.jensen@gmail.com>
2021-06-02 16:25:05 +0700

    added wallet balance to store.z

--- example/store.z/src/src/App/Context.tsx
+++ example/store.z/src/src/App/Context.tsx
@@ -3,6 +3,7 @@ import point from '../point'
 
 const defaultContext = {
   walletAddress: undefined as string | undefined,
+  walletBalance: undefined as string | undefined
 }
 
 const AppContext = createContext(defaultContext)
@@ -10,15 +11,18 @@ const AppContext = createContext(defaultContext)
 export const useAppContext = () => useContext(AppContext)
 export const ProvideAppContext = ({ children }: { children: ReactNode }) => {
   const [walletAddress, setWalletAddress] = useState<string>()
+  const [walletBalance, setWalletBalance] = useState<string>()
 
   useEffect(() => {
     (async () => {
       setWalletAddress(await window.point.wallet.getAddress())
+      setWalletBalance((await window.point.wallet.getBalance()).token)
     })()
   }, [])
 
   const context = {
     walletAddress,
+    walletBalance
   }
 
   return <AppContext.Provider value={ context }>{ children }</AppContext.Provider>
