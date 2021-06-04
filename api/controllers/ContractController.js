91dc5139578444cac8c00db950368af0ee10ee0e
Darren Jensen <darren.jensen@gmail.com>
2021-06-04 16:16:17 +0700

    fixed indenting in ContractController

--- api/controllers/ContractController.js
+++ api/controllers/ContractController.js
@@ -1,21 +1,21 @@
 const PointController = require('./PointController')
 
 class ContractController extends PointController {
-  constructor(ctx, request) {
-    super(ctx)
-    this.request = request
-  }
+    constructor(ctx, request) {
+        super(ctx)
+        this.request = request
+    }
 
-  async call() {
-    const host = this.request.query.host;
-    const contractName = this.request.query.contractName;
-    const method = this.request.query.method;
-    const params = this.request.query.params;
+    async call() {
+        const host = this.request.query.host;
+        const contractName = this.request.query.contractName;
+        const method = this.request.query.method;
+        const params = this.request.query.params;
 
-    let data = await this.ctx.web3bridge.callContract(host, contractName, method, params);
+        let data = await this.ctx.web3bridge.callContract(host, contractName, method, params);
 
-    return this._response(data);
-  }
-}
+        return this._response(data);
+        }
+    }
 
 module.exports = ContractController;
