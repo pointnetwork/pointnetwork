e1e6ad68cba1db6af3e39f270f861209ccf43d89
Darren Jensen <darren.jensen@gmail.com>
2021-06-11 11:07:03 +0700

    refactor contract controller to use wallet helpers

--- api/controllers/ContractController.js
+++ api/controllers/ContractController.js
@@ -1,11 +1,17 @@
 const PointController = require('./PointController')
+const helpers = require('./helpers/WalletHelpers')
 
 class ContractController extends PointController {
     constructor(ctx, req, reply) {
         super(ctx);
         this.req = req;
+        this.walletToken = this.req.headers['wallet-token'];
         this.payload = req.body;
         this.reply = reply;
+
+        // load wallet
+        const wallet = helpers.initWallet(ctx, req.headers['wallet-token'])
+        wallet ? this.wallet = wallet : this.reply.callNotFound()
     }
 
     async call() {
@@ -20,9 +26,7 @@ class ContractController extends PointController {
      }
 
      async send() {
-        this.walletToken = this.req.headers['wallet-token'];
-
-        if(this._loadWallet()) {
+        if(this.wallet) {
             const host = this.payload.host;
             const contractName = this.payload.contractName;
             const method = this.payload.method;
@@ -35,36 +39,6 @@ class ContractController extends PointController {
             return this._response(data);
         }
      }
-
-     /* Private Functions */
-
-    _validateWalletToken() {
-        if(this.walletToken === undefined) {
-            throw new Error('Missing wallet-token header.')
-        }
-        if(this.walletToken.length < 69) {
-            throw new Error('wallet-token invalid.')
-        }
-    }
-
-    _parseWalletToken() {
-        this._validateWalletToken();
-        this.walletId = this.walletToken.slice(0,36)
-        this.passcode = this.walletToken.slice(37, 103)
-    }
-
-    _loadWallet() {
-        this._parseWalletToken()
-        // load the wallet from the keystore file
-        this.wallet = this.ctx.wallet.loadWalletFromKeystore(this.walletId, this.passcode)
-        if(this.wallet) {
-            return true
-        } else {
-            // if wallet is null then reply with 404 not found and return false
-            this.reply.callNotFound()
-            return false
-        }
-    }
 }
 
 module.exports = ContractController;
