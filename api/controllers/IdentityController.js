23b95b69e7549a1608214a0ae02f71468e6c7697
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-07-14 17:43:09 -0700

    Ran eslint on file.

--- api/controllers/IdentityController.js
+++ api/controllers/IdentityController.js
@@ -1,23 +1,23 @@
-const PointSDKController = require('./PointSDKController')
+const PointSDKController = require('./PointSDKController');
 
 class IdentityController extends PointSDKController {
     constructor(ctx, req, rep) {
         super(ctx);
-	this.req = req
-	this.rep = rep
+        this.req = req;
+        this.rep = rep;
     }
 
     async identityToOwner() {
-	let identity = this.req.params.identity;
-	let owner = await this.ctx.web3bridge.ownerByIdentity(identity);
+        let identity = this.req.params.identity;
+        let owner = await this.ctx.web3bridge.ownerByIdentity(identity);
         return this._response(
             {'owner': owner}
         );
     }
 
     async ownerToIdentity() {
-	let owner = this.req.params.owner
-	let identity = await this.ctx.web3bridge.identityByOwner(owner);
+        let owner = this.req.params.owner;
+        let identity = await this.ctx.web3bridge.identityByOwner(owner);
         return this._response(
             {'identity': identity}
         );
