d5db2c5b5465c96a5ad8bb12ac1ac490fc5b1922
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-07-14 17:23:41 -0700

    Added endpoints for identity management.

---
+++ api/controllers/IdentityController.js
@@ -0,0 +1,27 @@
+const PointSDKController = require('./PointSDKController')
+
+class IdentityController extends PointSDKController {
+    constructor(ctx, req, rep) {
+        super(ctx);
+	this.req = req
+	this.rep = rep
+    }
+
+    async identityToOwner() {
+	let identity = this.req.params.identity;
+	let owner = await this.ctx.web3bridge.ownerByIdentity(identity);
+        return this._response(
+            {'owner': owner}
+        );
+    }
+
+    async ownerToIdentity() {
+	let owner = this.req.params.owner
+	let identity = await this.ctx.web3bridge.identityByOwner(owner);
+        return this._response(
+            {'identity': identity}
+        );
+    }
+}
+
+module.exports = IdentityController;
