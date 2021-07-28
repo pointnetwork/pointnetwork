3dec1b66f3332632970a6cf959a548a91d9d62f0
den <d.n.voropaev@gmail.com>
2021-07-29 01:05:38 +0300

    store.z now unsubscribes from contract events on home button click

--- example/store.z/src/App/Context.tsx
+++ example/store.z/src/App/Context.tsx
@@ -1,16 +1,22 @@
-import React, { createContext, ReactNode, useContext, useState, useEffect } from 'react'
+import React, { createContext, ReactNode, useContext, useState, useEffect, useCallback } from 'react';
+import { useLocation } from "wouter";
+import { Subscription } from '~/@types';
 
 const defaultContext = {
   walletAddress: undefined as string | undefined,
   walletError: undefined as Error | undefined,
-}
+  goHome: () => {},
+  addSubscription: (() => {}) as (s: Subscription) => void,
+};
 
-const AppContext = createContext(defaultContext)
+const AppContext = createContext(defaultContext);
 
 export const useAppContext = () => useContext(AppContext)
 export const ProvideAppContext = ({ children }: { children: ReactNode }) => {
   const [walletAddress, setWalletAddress] = useState<string>();
   const [walletError, setWallerError] = useState<Error>();
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>();
+  const [, setLocation] = useLocation();
 
   useEffect(() => {
     (async () => {
@@ -26,9 +32,26 @@ export const ProvideAppContext = ({ children }: { children: ReactNode }) => {
     })()
   }, [])
 
+  const addSubscription = useCallback((subscription: Subscription) => {
+    setSubscriptions((subscriptions) => (subscriptions || []).concat(subscription));
+  }, [setSubscriptions]);
+
+  const goHome = useCallback(async () => {
+    if (subscriptions && subscriptions.length) {
+      for (const subscription of subscriptions) {
+        await subscription.unsubscribe();
+      }
+    }
+
+    setSubscriptions([]);
+    setLocation('/');
+  }, [subscriptions]);
+
   const context = {
     walletAddress,
     walletError,
+    addSubscription,
+    goHome
   }
 
   return <AppContext.Provider value={ context }>{ children }</AppContext.Provider>
