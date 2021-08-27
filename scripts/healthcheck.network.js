6e2c4f7cba78d3a3131dda30efde0571959ff62b
den <d.n.voropaev@gmail.com>
2021-08-27 19:59:23 +0300

    added network health check fixes

--- scripts/healthcheck.network.js
+++ scripts/healthcheck.network.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Web3 = require('web3');
 const {merge} = require('lodash')
 const levelup = require('levelup');
 const leveldown = require('leveldown');
@@ -6,20 +7,20 @@ const encode = require('encoding-down');
 const ethUtil = require('ethereumjs-util');
 const kadence = require('@pointnetwork/kadence');
 const Messenger = require('@pointnetwork/kadence/lib/messenger');
+const Kademlia = require('../network/kademlia');
 const utils = require('../core/utils');
 const AuthenticatePlugin = require('../network/kademlia/plugin-authenticate');
 const SerializerBSON = require('../network/kademlia/serializer-bson');
-const { id } = require('../db/models/chunk');
 const datadir = process.env.DATADIR;
 const config = merge(
   require(path.join(__dirname, '..', 'resources', 'defaultConfig.json')),
   require(path.join(datadir, 'config.json'))
 );
 
-const networkPrivateKeyHex = 'baecf1f685ff378c247d1b9c7095815a4604f03f1759a418e82df05b8c3be73a';
-const networkPrivateKey = Buffer.from(networkPrivateKeyHex, 'hex');
+const networkPrivateKey = Buffer.from(new Web3().eth.accounts.create().privateKey.replace(/^0x/, ''), 'hex');
 const networkPublicKey = ethUtil.privateToPublic(networkPrivateKey);
-const identity = ethUtil.privateToAddress(networkPrivateKey);
+const address = ethUtil.privateToAddress(networkPrivateKey);
+const identity = address;
 const tempPort = 1337;
 const hostname = config.network.communication_external_host;
 const protocol = parseInt(config.network.ssl_enabled) ? 'https:' : 'http:'
@@ -29,8 +30,10 @@ const messenger = new Messenger({
     deserializer: SerializerBSON.deserializer
 });
 
+Kademlia.prototype.patchKadence();
+
 const node = new kadence.KademliaNode({
-    identity: identity,
+    identity,
     storage: levelup(encode(leveldown(path.join(datadir, 'healthcheck')))),
     messenger,
     logger: console,
@@ -38,36 +41,24 @@ const node = new kadence.KademliaNode({
     contact
 });
 
-node.authenticate = node.plugin(AuthenticatePlugin({config, utils, log: console}, networkPublicKey, networkPrivateKey, {
-    privateKey: networkPrivateKey,
-    publicKey: networkPublicKey
-}));
-
-node.rolodex = node.plugin(kadence.rolodex(path.join(datadir, config.network.peer_cache_file_path)));
+node.authenticate = node.plugin(AuthenticatePlugin({config, utils, log: console}, networkPublicKey, networkPrivateKey));
 
 const targetUrl = `${protocol}//${hostname}:${config.network.communication_port}/#${
     ethUtil.privateToAddress(Buffer.from(config.client.wallet.privateKey, 'hex')).toString('hex')
 }`;
 
 const target = kadence.utils.parseContactURL(targetUrl);
+const checkHealth = async () => {
+    try {
+        // console.info({listen: await new Promise((resolve) => node.listen(tempPort, resolve))});
+        console.info({join: await node.join(target)});
+        console.info({ping: await node.ping(target)});
+        console.info({close: await node.send('DISCONNECT', [], target)});
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
 
-console.log({
-    target,
-    hostname,
-    protocol,
-    port: config.network.communication_port,
-    targetUrl
-});
-
-node.listen(tempPort, () => {
-    node.join(target, (err) => {
-        console.error({err})
-        if (err) {
-            process.exit(1);
-        }
-        node.send('PING', [], target, (err, response) => {
-            console.log('PING:', {err, response});
-            process.exit(err ? 1 : 0);
-        });
-    });
-});
+checkHealth();
