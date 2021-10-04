6910fa9502e45632a8f009424cadcfb40cbaa551
den <d.n.voropaev@gmail.com>
2021-10-04 15:53:06 +0300

    added a neon-proxy hack to increase the stored data limit

--- scripts/handle-neon-proxy.sh
+++ scripts/handle-neon-proxy.sh
@@ -9,6 +9,11 @@ DEPLOY_TRIGGER=deploy
 [[ -z $ETH_TOKEN_MINT || $ETH_TOKEN_MINT == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
 [[ -z $COLLATERAL_POOL_BASE || $COLLATERAL_POOL_BASE == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
 
+sed -i 's/code_account_size = CODE_INFO_LAYOUT.sizeof() + code_size + valids_size + 2048/code_account_size = CODE_INFO_LAYOUT.sizeof() + code_size + valids_size + 9437184/g' /opt/proxy/plugin/solana_rest_api_tools.py
+sed -i 's/code_account_size = CODE_INFO_LAYOUT.sizeof() + msg_size + valids_size + 2048/code_account_size = CODE_INFO_LAYOUT.sizeof() + msg_size + valids_size + 9437184/g' /opt/proxy/plugin/solana_rest_api_tools.py
+
+sed -n -e 666p -e 897p /opt/proxy/plugin/solana_rest_api_tools.py
+
 if [ -z $DEPLOY ]; then
   $RUNNABLE
 else
