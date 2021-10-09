2bade83eca0e7a2d6ecf035256d7a4c08b9e944a
den <d.n.voropaev@gmail.com>
2021-10-09 22:25:28 +0300

    updated identity contract and bumped point node version

--- truffle/contracts/Identity.sol
+++ truffle/contracts/Identity.sol
@@ -23,21 +23,7 @@ contract Identity {
     event IKVSet(string identity, string key, string value);
 
     constructor() {
-        _selfReg('storage_provider', 0xC01011611e3501C6b3F6dC4B6d3FE644d21aB301, PubKey64({part1: 0xb192de79890d7dd86de3eb3343e16b873f7acff3cf9c49c4587337f06b744b41, part2: 0x6a34054dd05f0e6ffd720bf61e3aa1858bff9e1e95df22469f6b693fc9725376}));
-        // 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB is node2, deployer
-        _selfReg('demo', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('email', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('blog', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('hello', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('profile', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('twitter', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('store', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('pointsocial', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('explorer', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('zengarden', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
-        _selfReg('template', 0x4f5877E51067d0d68784aA74C39871cb2eF2D9eB, PubKey64({part1: 0x5befb2b9737edb839afde5ded2b8187aa17f640c5a953f726242d50cc3b18a22, part2: 0x8cf4b8a2e249f236fc50362194dd524d25f458e9fcf3a70ce36904ab85316f56}));
 
-        _selfReg('node3', 0xf990AB98B33dd48dffaC735C572D6cd8f75E60d8, PubKey64({part1: 0x1b26e2c556ae71c60dad094aa839162117b28a462fc4c940f9d12675d3ddfff2, part2: 0xaeef60444a96a46abf3ca0a420ef31bff9f4a0ddefe1f80b0c133b85674fff34}));
     }
 
     function _selfReg(string memory handle, address owner, PubKey64 memory commPublicKey) internal {
@@ -65,7 +51,7 @@ contract Identity {
         if (!_isEmptyString(lowercaseToCanonicalIdentities[lowercase])) revert('This identity has already been registered');
 
         // Check if this owner already has an identity attached
-        if (!_isEmptyString(ownerToIdentity[identityOwner])) revert('This owner already has an identity attached');
+        // if (!_isEmptyString(ownerToIdentity[identityOwner])) revert('This owner already has an identity attached');
 
         _selfReg(handle, identityOwner, commPublicKey);
     }
