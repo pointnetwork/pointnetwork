c6ae3b266ccf97423c0e67005c9398cdfac82a5d
den <d.n.voropaev@gmail.com>
2021-12-08 00:24:27 +0300

    updated pointsocial comments injection

--- example/pointsocial.z/contracts/PointSocial.sol
+++ example/pointsocial.z/contracts/PointSocial.sol
@@ -177,40 +177,41 @@ contract PointSocial  {
             postById[_posts[i].id] = _posts[i];
         }
 
-        // comment 1 on post 1
-        _commentIds.increment();
-        Comment memory comment1 = Comment({
+        Comment[3] memory comments = [Comment({
             id: 1,
             from: 0x314dF55775e0b6F2B0c6d07C7Ec83a3e1cdC165e,
             contents: 0xb87a664b88b76657f5e91ca6a50af72c2d27d2949fc85fd07528fdf0298d5953,
             createdAt: 1634132804
-        });
-        commentsByPost[1].push(comment1);
-        // commentById[comment1.id] = comment1;
-        // commentsByOwner[comment1.from].push(comment1);
+        }), Comment({
+            id: 2,
+            from: 0xB183c65f61BE14C77e31079A685ddeDEfF9937Ad,
+            contents: 0xd8f4902ce8afaab5381cca38732a6cc4b784dfae7735c0efdcc1ed58ab70fcad,
+            createdAt: 1634284869
+        }), Comment({
+            id: 3,
+            from: 0x8690481754AaFF7f6cC51EeA188a1903C5E451b4,
+            contents: 0x873a1dcee5a00bb350f24e0cdfcbcc1d34144abe8dde37a5ff67ed3a1f5cd534,
+            createdAt: 1634294379
+        })];
+
+        // comment 1 on post 1
+        commentsByPost[1].push(comments[0]);
+        commentById[comments[0].id] = comments[0];
+        commentsByOwner[comments[0].from].push(comments[0]);
 
         // comment 2 on post 5
-        // _commentIds.increment();
-        // commentsByPost[5].push(Comment({
-        //     id: 2,
-        //     from: 0xB183c65f61BE14C77e31079A685ddeDEfF9937Ad,
-        //     contents: 0xd8f4902ce8afaab5381cca38732a6cc4b784dfae7735c0efdcc1ed58ab70fcad,
-        //     createdAt: 1634284869
-        // }));
-        // // commentById[comment2.id] = comment2;
-        // // commentsByOwner[comment2.from].push(comment2);
-
+        commentsByPost[5].push(comments[1]);
+        commentById[comments[1].id] = comments[1];
+        commentsByOwner[comments[1].from].push(comments[1]);
 
         // comment 3 on post 6
-        // _commentIds.increment();
-        // commentsByPost[6].push(Comment({
-        //     id: 3,
-        //     from: 0x8690481754AaFF7f6cC51EeA188a1903C5E451b4,
-        //     contents: 0x873a1dcee5a00bb350f24e0cdfcbcc1d34144abe8dde37a5ff67ed3a1f5cd534,
-        //     createdAt: 1634294379
-        // }));
-        // commentById[comment3.id] = comment3;
-        // commentsByOwner[comment3.from].push(comment3);
+        commentsByPost[6].push(comments[2]);
+        commentById[comments[2].id] = comments[2];
+        commentsByOwner[comments[2].from].push(comments[2]);
+
+        for (uint i = 0; i < 3; i++) {
+            _commentIds.increment();
+        }
     }
 
     // Post data functions
