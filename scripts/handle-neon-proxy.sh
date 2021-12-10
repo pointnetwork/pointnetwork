8322ca9e16727016d93dd295b3ebbeacef7bbb85
den <d.n.voropaev@gmail.com>
2021-12-10 13:16:33 +0300

    updated neon patch script

--- scripts/handle-neon-proxy.sh
+++ scripts/handle-neon-proxy.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-ENV_FILE=/opt/env/.env
-RUNNABLE=/opt/proxy/run-proxy.sh
-DEPLOY_TRIGGER=deploy
+# It's all different in the `devnet`
 
-[[ -f $ENV_FILE ]] && source $ENV_FILE
-[[ -z $EVM_LOADER || $EVM_LOADER == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
-[[ -z $ETH_TOKEN_MINT || $ETH_TOKEN_MINT == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
-[[ -z $COLLATERAL_POOL_BASE || $COLLATERAL_POOL_BASE == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
+# ENV_FILE=/opt/env/.env
+# RUNNABLE=/opt/proxy/run-proxy.sh
+# DEPLOY_TRIGGER=deploy
 
-sed -i 's/code_account_size = CODE_INFO_LAYOUT.sizeof() + code_size + valids_size + 2048/code_account_size = CODE_INFO_LAYOUT.sizeof() + code_size + valids_size + 9437184/g' /opt/proxy/plugin/solana_rest_api_tools.py
-sed -i 's/code_account_size = CODE_INFO_LAYOUT.sizeof() + msg_size + valids_size + 2048/code_account_size = CODE_INFO_LAYOUT.sizeof() + msg_size + valids_size + 9437184/g' /opt/proxy/plugin/solana_rest_api_tools.py
+# [[ -f $ENV_FILE ]] && source $ENV_FILE
+# [[ -z $EVM_LOADER || $EVM_LOADER == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
+# [[ -z $ETH_TOKEN_MINT || $ETH_TOKEN_MINT == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
+# [[ -z $COLLATERAL_POOL_BASE || $COLLATERAL_POOL_BASE == "$DEPLOY_TRIGGER" ]] && DEPLOY=1
 
-# sed -n -e 666p -e 897p /opt/proxy/plugin/solana_rest_api_tools.py
+FILE_TO_PATCH="/opt/proxy/plugin/solana_rest_api_tools.py"
 
-if [ -z $DEPLOY ]; then
-  $RUNNABLE
-else
-  $RUNNABLE | while read LINE; do
-    echo $LINE
-    [[ $LINE == *"EVM_LOADER="* || $LINE == *"ETH_TOKEN_MINT="* || $LINE == *"COLLATERAL_POOL_BASE="* ]] && echo "export $LINE" >> $ENV_FILE
-  done
-fi
+echo
+echo "Patching Neon Proxy Solana REST API tools..."
+sed -i '871s/2048/9437184/' $FILE_TO_PATCH
+echo "Successfully patched Neon Proxy Solana REST API tools:"
+sed -n -e 871p $FILE_TO_PATCH
+echo
+echo
+
+/opt/proxy/run-proxy.sh
+
+# if [ -z $DEPLOY ]; then
+#   $RUNNABLE
+# else
+#   $RUNNABLE | while read LINE; do
+#     echo $LINE
+#     [[ $LINE == *"EVM_LOADER="* || $LINE == *"ETH_TOKEN_MINT="* || $LINE == *"COLLATERAL_POOL_BASE="* ]] && echo "export $LINE" >> $ENV_FILE
+#   done
+# fi
