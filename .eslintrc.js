0066d6abd01e14872acf3e2f0f2b0e37658cee0a
den <d.n.voropaev@gmail.com>
2022-01-26 23:18:39 +0300

    updated eslint rules, added required packages, removed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,59 @@
 module.exports = {
-    'env': {
-        'node': true,
-        'es2021': true,
-        'mocha': true
+    env: {
+        node: true,
+        es2021: true,
+        mocha: true
     },
-    'extends': [
+    extends: [
         // 'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'prettier',
-        'plugin:import/recommended'
+        // 'plugin:import/recommended'``
     ],
     // 'parser': '@typescript-eslint/parser',
-    'plugins': [
+    plugins: [
         // '@typescript-eslint',
-        'prettier',
-        'import',
+        // 'import',
         'filenames',
         'prefer-arrow'
     ],
-    'ignorePatterns': [],
-    'rules': {
+    ignorePatterns: [],
+    rules: {
         'no-implicit-coercion': ['error'],
-        'indent': ['error', 4, {'SwitchCase': 1}], // This should be in sync with prettier config
+        'indent': ['error', 4, {
+            SwitchCase: 1,
+            MemberExpression: 1,
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            VariableDeclarator: 1,
+            CallExpression: {arguments: 1},
+            offsetTernaryExpressions: true
+        }],
         'semi': ['error', 'always'],
-        'prettier/prettier': ['error', {'usePrettierrc': true}],
-        'indent': 'off',
         'linebreak-style': ['error', 'unix'],
         'no-fallthrough': 'off',
         'no-console': 'error',
         'no-debugger': 'error',
         'no-unused-vars': 'error',
         'max-len': ['warn', {
-            'code': 100, // This should be in sync with prettier config
-            'ignoreStrings': true,
-            'ignoreRegExpLiterals': true,
-            'ignoreTemplateLiterals': true,
-            'tabWidth': 4,
-            'ignoreComments': false
+            code: 100,
+            ignoreStrings: true,
+            ignoreRegExpLiterals: true,
+            ignoreTemplateLiterals: true,
+            tabWidth: 4,
+            ignoreComments: false
         }],
-        'comma-dangle': ['error', 'never'], // This should be in sync with prettier config
-        'space-in-parens': ['error', 'never'], // This should be in sync with prettier config
+        'comma-dangle': ['error', 'never'],
+        'space-in-parens': ['error', 'never'],
         'comma-spacing': 'error',
-        'object-curly-spacing': ['error', 'never'], // This should be in sync with prettier config
-        'array-bracket-spacing': ['error', 'never'], // This should be in sync with prettier config
+        'object-curly-spacing': ['error', 'never'],
+        'object-curly-newline': ['error', {multiline: true}],
+        'array-bracket-spacing': ['error', 'never'],
         'eqeqeq': ['error', 'always'],
         'no-useless-call': 'error',
         'prefer-promise-reject-errors': 'error',
         'no-underscore-dangle': 'error',
-        'prefer-const': ['error', {'destructuring': 'all', 'ignoreReadBeforeAssign': false}],
-        'import/no-extraneous-dependencies': 'error',
-        'import/imports-first': ['error', 'absolute-first'],
-        'import/extensions': ['error', 'never'],
-        'import/no-unresolved': 'off',
-        'filenames/match-regex': 'error',
-        'filenames/match-exported': 'error',
+        'prefer-const': ['error', {destructuring: 'all', ignoreReadBeforeAssign: false}],
+        // 'filenames/match-regex': 'error',
+        // 'filenames/match-exported': 'error',
         'no-bitwise': 'off',
         'no-caller': 'error',
         'no-useless-return': 'error',
@@ -61,20 +61,26 @@ module.exports = {
         'no-unused-expressions': 'error',
         'arrow-body-style': ['error', 'as-needed'],
         'prefer-arrow/prefer-arrow-functions': ['error', {
-            'disallowPrototype': true,
-            'singleReturnOnly': false,
-            'classPropertiesAllowed': false
+            disallowPrototype: true,
+            singleReturnOnly: false,
+            classPropertiesAllowed: false
         }],
-        'brace-style': ['error', '1tbs', {'allowSingleLine': true}],
+        'brace-style': ['error', '1tbs', {allowSingleLine: true}],
         'space-infix-ops': ['error'],
-        'quote-props': ['error', 'consistent-as-needed'], // This should be in sync with prettier config
-        'no-multiple-empty-lines': ['warn', {'max': 1, 'maxEOF': 0}],
-        'quotes': ['error', 'single', {'allowTemplateLiterals': true}]
+        'quote-props': ['error', 'consistent-as-needed'],
+        'no-multiple-empty-lines': ['warn', {max: 1, maxEOF: 0}],
+        'quotes': ['error', 'single', {allowTemplateLiterals: true}],
+        'no-eval': 'error',
+        'no-implied-eval': 'error'
         // '@typescript-eslint/no-var-requires': 'error', // If we decide to proceed with TS
         // '@typescript-eslint/ban-ts-comment': 'error', // If we decide to proceed with TS
         // '@typescript-eslint/no-non-null-assertion': 'off', // If we decide to proceed with TS
         // '@typescript-eslint/no-shadow': 'error',
         // '@typescript-eslint/no-unused-vars': 'error'
         // 'no-shadow': 'off',
+        // 'import/no-extraneous-dependencies': 'error',
+        // 'import/imports-first': ['error', 'absolute-first'],
+        // 'import/extensions': ['error', 'never'],
+        // 'import/no-unresolved': 'off',
     }
-}
+};
