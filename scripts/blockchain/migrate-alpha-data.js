72dfbc86c72ab59f37ccf11b1834be0cada115c9
Alexandre Melo <amelo@harlemnext.com>
2022-02-14 08:51:59 -0300

    fixing PR

--- scripts/blockchain/migrate-alpha-data.js
+++ scripts/blockchain/migrate-alpha-data.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 //node scripts/blockchain/migrate-alpha-data.js twitter.z
 
-const {readFileSync} = require("fs");
-const Web3 = require("web3");
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const {readFileSync} = require('fs');
+const Web3 = require('web3');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const blockchainUrl = process.env.BLOCKCHAIN_URL;
-const pointNodePath = "/app"
+const pointNodePath = '/app'
 const nodeConfigFile = `${process.env.DATADIR}/config.json`;
-const Web3HttpProvider = require("web3-providers-http");
+const Web3HttpProvider = require('web3-providers-http');
 
-const mnemonic = require("/data/keystore/key.json");
-const {exit} = require("process");
-if (typeof mnemonic !== "object" || !("phrase" in mnemonic)) {
-    throw new Error("Invalid key format");
+const mnemonic = require('/data/keystore/key.json');
+const {exit} = require('process');
+if (typeof mnemonic !== 'object' || !('phrase' in mnemonic)) {
+    throw new Error('Invalid key format');
 }
 const httpProvider = new Web3HttpProvider(blockchainUrl, {keepAlive: true, timeout: 60000});
 const privateKeyProvider = new HDWalletProvider({mnemonic, providerOrUrl: httpProvider});
-const privateKey = `0x${privateKeyProvider.hdwallet._hdkey._privateKey.toString("hex")}`;
+const privateKey = `0x${privateKeyProvider.hdwallet._hdkey._privateKey.toString('hex')}`;
 const hdWalletProvider = new HDWalletProvider({
     privateKeys: [privateKey], 
     providerOrUrl: blockchainUrl
@@ -31,36 +31,36 @@ let config;
 let identity_contract_address;
 
 const allowedSites = {
-    "twitter.z": {
-        "abi":`${pointNodePath}/truffle/build/contracts/TwitterMigrations.json`,
-        "contract":"0x9EF1BEcF1e63BAB97F883bA3C5fDf8f761eD41F8",
-        "handle":"twitter",
+    'twitter.z': {
+        'abi':`${pointNodePath}/truffle/build/contracts/TwitterMigrations.json`,
+        'contract':'0x9EF1BEcF1e63BAB97F883bA3C5fDf8f761eD41F8',
+        'handle':'twitter'
     }, 
-    "blog.z": {
-        "abi":`${pointNodePath}/truffle/build/contracts/BlogMigrations.json`,
-        "contract":"0x506d6A079856A6EF444c99dAef1bf15fcbc37bbb",
-        "handle":"blog",
+    'blog.z': {
+        'ab':`${pointNodePath}/truffle/build/contracts/BlogMigrations.json`,
+        'contract':'0x506d6A079856A6EF444c99dAef1bf15fcbc37bbb',
+        'handle':'blog'
     }, 
-    "pointsocial.z": {
-        "abi":`${pointNodePath}/truffle/build/contracts/PointSocialMigrations.json`,
-        "contract":"0x59BDA94F762c9227cC426Ea29C0aC1c196f1d7b6",
-        "handle":"pointsocial",
+    'pointsocial.z': {
+        'abi':`${pointNodePath}/truffle/build/contracts/PointSocialMigrations.json`,
+        'contract':'0x59BDA94F762c9227cC426Ea29C0aC1c196f1d7b6',
+        'handle':'pointsocial'
     }, 
 };
 
 const minimal_contract_abi = [
     {
-        "inputs":[
+        'inputs':[
            {
-              "internalType":"address",
-              "name":"migrator",
-              "type":"address"
+              'internalType':'address',
+              'name':'migrator',
+              'type':'address'
            }
         ],
-        "name":"addMigrator",
-        "outputs":[],
-        "stateMutability":"nonpayable",
-        "type":"function"
+        'name':'addMigrator',
+        'outputs':[],
+        'stateMutability':'nonpayable',
+        'type':'function'
      }
 ];
 
@@ -77,7 +77,7 @@ const init = () => {
     storage_provider_registry_contract_address = config.network.storage_provider_registry_contract_address;
 
     if(allowedSites[site] == undefined) {
-        console.log("Please inform a valid zapp domain");
+        console.log('Please inform a valid zapp domain');
         exit(0);
     }
     
@@ -95,8 +95,8 @@ const startMigration = async(site) => {
 
     const handleAddress = await identityInstance.methods.getOwnerByIdentity(handle).call({from:account.address});
 
-    if(handleAddress == "0x0000000000000000000000000000000000000000") {
-        console.log("Please deplot your zapp before running their migrations");
+    if(handleAddress == '0x0000000000000000000000000000000000000000') {
+        console.log('Please deplot your zapp before running their migrations');
         exit(0);
     }
 
@@ -114,7 +114,7 @@ const startMigration = async(site) => {
         console.log(txRaw)
     });
     
-    console.log("Migrator added");    
+    console.log('Migrator added');    
     await migratorInstance.methods.migrate(contractAddress).send(
     {
         from: account.address,
@@ -123,7 +123,7 @@ const startMigration = async(site) => {
         console.log(txRaw)
     });
 
-    console.log("Migrated");    
+    console.log('Migrated');    
     exit(0);
 }
 
