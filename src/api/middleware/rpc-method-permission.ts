822482f569e6c9c8555b170cc640e03301800e88
German Viescas <german@pointlabs.org>
2022-04-05 09:35:58 -0300

    PN-113 Check `req.body` is defined in middleware.

--- src/api/middleware/rpc-method-permission.ts
+++ src/api/middleware/rpc-method-permission.ts
@@ -6,27 +6,29 @@ import permissionStore from '../../permissions/PermissionStore';
 const RESTRICTED_METHODS = ['eth_sendTransaction'];
 
 async function methodPermissionMdw(req: FastifyRequest, reply: FastifyReply) {
-    const {method} = req.body as RPCRequestBody;
+    if (req.body) {
+        const {method} = req.body as RPCRequestBody;
 
-    if (RESTRICTED_METHODS.includes(method)) {
-        const dappDomain = req.headers.origin;
-        if (!dappDomain) {
-            reply.status(400).send({
-                code: 400,
-                message: '`Origin` header is required.'
-            });
-            return reply;
-        }
+        if (RESTRICTED_METHODS.includes(method)) {
+            const dappDomain = req.headers.origin;
+            if (!dappDomain) {
+                reply.status(400).send({
+                    code: 400,
+                    message: '`Origin` header is required.'
+                });
+                return reply;
+            }
 
-        const address = blockchain.getOwner();
-        const permissions = await permissionStore.get(dappDomain, address);
-        if (!permissions || !permissions.parentCapabilities.includes(method)) {
-            reply.status(401).send({
-                code: 4100,
-                name: 'Unauthorized',
-                message: `Permission for method "${method}" has not been granted.`
-            });
-            return reply;
+            const address = blockchain.getOwner();
+            const permissions = await permissionStore.get(dappDomain, address);
+            if (!permissions || !permissions.parentCapabilities.includes(method)) {
+                reply.status(401).send({
+                    code: 4100,
+                    name: 'Unauthorized',
+                    message: `Permission for method "${method}" has not been granted.`
+                });
+                return reply;
+            }
         }
     }
 }
