87d934cdb3f192cd73cc784c33eb318f1d3626ce
Denis Voropaev <d.n.voropaev@gmail.com>
2022-04-19 19:36:37 +0300

    fixed routes

--- src/client/proxy/handlers/index.ts
+++ src/client/proxy/handlers/index.ts
@@ -4,8 +4,6 @@ import attachStorageHandlers from './storage';
 import attachKeyValueHanlders from './keyValue';
 import attachContractSendHandler from './contractSend';
 import attachApiHandler from './api';
-import attachWebsocketHanlders from './websocket';
-
 
 // TODO: ctx is needed for Renderer and keyvalue, remove it later
 const attachHandlers = (server: FastifyInstance, ctx: any) => {
@@ -14,7 +12,6 @@ const attachHandlers = (server: FastifyInstance, ctx: any) => {
     attachContractSendHandler(server);
     attachKeyValueHanlders(server);
     attachCommonHandler(server, ctx);
-    attachWebsocketHanlders(server, ctx);
 };
 
 export default attachHandlers;
