f70a6fd79eaa0b73bf809131c05bd53d8e5b7e49
Nicolas Bevilacqua <nicobevilacqua@gmail.com>
2022-05-09 10:42:40 -0300

    jquery removed

--- internal/explorer.point/src/pages/Final.jsx
+++ internal/explorer.point/src/pages/Final.jsx
@@ -1,85 +1,94 @@
+import React, { useRef, useState } from 'react';
 import Container from 'react-bootstrap/Container'
-import $ from 'jquery';
 import Swal from 'sweetalert2';
+import axios from 'axios'; 
 
-export default function Final() {
+const Final = () => {
+    const [identity, setIdentity] = useState('');
+    const [error, setError] = useState('');
+    const [available, setAvailable] = useState(false);
 
+    function validate_identity(identity) {
+        if (identity === '') {
+            setError('empty identity');
+            return;
+        }
+            
+        if (! /^[a-zA-Z0-9]+?$/.test(identity)) {
+            setError('special characters are not allowed');
+            return;
+        }
 
-    function register(identity) {
+        if (identity.length > 16) {
+            setError('handle is too long');
+            return;
+        }
+
+        return true;
+    }
+
+    const source = useRef(axios.CancelToken.source());
+    let debounced = useRef(null);
+    const onChangeHandler = (event) => {
+        const identity = event.target.value;
+        if (!validate_identity(identity)) {
+            return;
+        }
+        setIdentity('');
+        clearTimeout(debounced.current);
+        debounced.current = setTimeout(() => {
+            setError('');
+            axios.get(`/v1/api/identity/identityToOwner/${identity}`, {
+                cancelToken: source.current.token
+            }).then(({ data }) => {
+                const owner = (data || {}).owner;
+                setIdentity(identity);
+                setAvailable(!owner || owner === "0x0000000000000000000000000000000000000000");
+            }).catch((thrown) => {
+                if (!axios.isCancel(thrown)) {
+                    console.error(thrown);
+                    setError('Something went wrong')
+                }
+            })
+        }, 300);
+    } 
+
+    const registerHandler = () => {
         Swal.fire({
             title: 'Are you sure you want to be known as '+identity+'?',
             showCancelButton: true,
             confirmButtonText: 'Sure!',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                const csrf_token = window.localStorage.getItem('csrf_token');
-                $.ajax({
-                    url: '/v1/api/identity/register',
-                    type: 'POST',
-                    contentType: 'application/json; charset=utf-8',
-                    dataType: 'json',
-                    data: `{"identity": "${identity}", "_csrf": "${csrf_token}"}`,
-                    success: () => {
-                        window.location = '/';
-                    },
-                    error: err => {
-                        console.error(err);
-                        Swal.fire({title: 'Something went wrong'});
-                    }
-                });
+        }).then(({ isConfirmed }) => {
+            if (!isConfirmed) {
+                return;
             }
+
+            const csrf_token = window.localStorage.getItem('csrf_token');
+
+            axios({
+                url: '/v1/api/identity/register',
+                method: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                data: {
+                    identity,
+                    _csrf: csrf_token,
+                },
+            }).then(() => {
+                window.location = '/';
+            }).catch((error) => {
+                console.error(error);
+                Swal.fire({title: 'Something went wrong'});
+            });
         })
     }
 
-    function green() {
-        $('#handle').css('border-color', 'green');
-        $('#result').css('color', 'green');
-        $('#finalize').show();
+    let resultStyles = {};
+    if (error) {
+        resultStyles = { borderColor: 'red', color: 'red' };
+    } else if (available) {
+        resultStyles = { borderColor: 'green', color: 'green' };
     }
-    function red() {
-        $('#handle').css('border-color', 'red');
-        $('#result').css('color', 'red');
-        $('#finalize').hide();
-    }
-
-    function available(identity) {
-        green();
-        $('#result').text(identity + ' is available');
-    }
-
-    function not_available(identity) {
-        red();
-        $('#result').text(identity + ' is not available');
-    }
-
-    function no(why) {
-        red(); $('#result').text(why);
-        return false;
-    }
-
-    function validate_identity(identity) {
-        if (identity === '') return no('empty identity');
-        if (! /^[a-zA-Z0-9]+?$/.test(identity)) return no('special characters are not allowed');
-        if (identity.length > 16) return no('handle is too long');
-        return true;
-    }
-
-    $(function() {
-        $('#handle').keyup(function(e, a) {
-            const identity = $('#handle').val();
-            if (!validate_identity(identity)) return;
-            $.get(`/v1/api/identity/identityToOwner/${identity}`, function(data) {
-                if (
-                    !(data && data.data && data.data.owner)
-                    || data.data.owner === "0x0000000000000000000000000000000000000000"
-                ) {
-                    available(identity);
-                } else {
-                    not_available(identity);
-                }
-            });
-        });
-    });
 
     return (
         <Container className="p-3">
@@ -87,16 +96,19 @@ export default function Final() {
             <h1>Final step</h1>
             <p>Introduce yourself to the world by registering an identity, which will be your public web3 handle:</p>
 
-            <input type="text" name="handle" id="handle" />
+            <input type="text" name="handle" id="handle" onChange={onChangeHandler} />
 
-            <div id="result"></div>
+            <div id="result" style={resultStyles}>
+                {error ? error : identity ? `${identity} ${available ? 'is available' : 'is not available'}`  : ''}
+            </div>
+            
             <br/>
 
-            <div id="finalize" style={{display: 'none'}} >
-                <button className="btn btn-info" onClick={() => register($('#handle').val())}>Register</button>
-            </div>
-
+            {identity && available && !error ? (<div>
+                <button className="btn btn-info" onClick={registerHandler}>Register</button>
+            </div>) : ''}
         </Container>
     )
-
 }
+
+export default Final;
