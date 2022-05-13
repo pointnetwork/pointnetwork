96caa64349c31b782a0136bfd5f9d497bc8a8c7c
German Viescas <german@pointlabs.org>
2022-05-13 17:56:08 -0300

    Fix e2e test suite
    Two changes required to fix tests:
      - update tests to use ynet_ prefix
      - properly handle the response from Bundler when downloading a file,
      which is sent as a stream

--- tests/docker/blockchain.spec.js
+++ tests/docker/blockchain.spec.js
@@ -3,6 +3,7 @@ import {readFile} from 'fs/promises';
 import {get, post} from 'axios';
 import {delay, getContractAddress} from '../../src/util';
 import HttpsAgent from 'https-proxy-agent';
+import {TIMEOUTS} from '../timeouts';
 
 const DOCKER_POINT_NODE = 'point_node';
 const POINT_NODE = process.env.TEST_POINT_NODE || DOCKER_POINT_NODE;
@@ -35,7 +36,13 @@ beforeAll(async () => {
 
     testData.address = getNetworkAddress();
     testData.commPublicKey = getNetworkPublicKey();
-    const filePath = path.join(__dirname, '..', 'resources', 'sample_site', 'point.deploy.json');
+    const filePath = path.join(
+        __dirname,
+        '..',
+        'resources',
+        'ynet_sample_site',
+        'point.deploy.json'
+    );
     const deployConfigFile = await readFile(filePath, 'utf-8');
     testData.deployConfig = JSON.parse(deployConfigFile);
 });
@@ -46,168 +53,206 @@ describe('Register identity and deploy site', () => {
     let publicDirStorageId;
     let routesStorageId;
 
-    it('Should register a new identity', async () => {
-        expect.assertions(5);
+    it(
+        'Should register a new identity',
+        async () => {
+            expect.assertions(5);
 
-        const {target} = testData.deployConfig;
-        const identity = target.replace(/\.point$/, '');
+            const {target} = testData.deployConfig;
+            const identity = target.replace(/\.point$/, '');
 
-        const result = await blockchain.registerIdentity(
-            identity,
-            testData.address,
-            Buffer.from(testData.commPublicKey, 'hex')
-        );
+            const result = await blockchain.registerIdentity(
+                identity,
+                testData.address,
+                Buffer.from(testData.commPublicKey, 'hex')
+            );
 
-        expect(result.events.IdentityRegistered).toBeTruthy();
-        expect(result.from).toEqual(testData.address);
-        expect(result.blockNumber).toBeGreaterThan(0);
-        expect(result.gasUsed).toBeGreaterThan(0);
-        expect(result.status).toBe(true);
-    }, 20000);
+            expect(result.events.IdentityRegistered).toBeTruthy();
+            expect(result.from).toEqual(testData.address);
+            expect(result.blockNumber).toBeGreaterThan(0);
+            expect(result.gasUsed).toBeGreaterThan(0);
+            expect(result.status).toBe(true);
+        },
+        TIMEOUTS.SM
+    );
 
-    it('Should deploy the sample site to arweave', async () => {
-        expect.assertions(1);
-        const publicDir = path.join(__dirname, '..', 'resources', 'sample_site', 'public');
-        publicDirStorageId = await storage.uploadDir(publicDir);
-        const {target, version} = testData.deployConfig;
-        await blockchain.putKeyValue(target, '::rootDir', publicDirStorageId, version);
-        expect(publicDirStorageId).toMatch(hexRegExp);
-    }, 120000);
+    it(
+        'Should deploy the sample site to arweave',
+        async () => {
+            expect.assertions(1);
+            const publicDir = path.join(__dirname, '..', 'resources', 'ynet_sample_site', 'public');
+            publicDirStorageId = await storage.uploadDir(publicDir);
+            const {target, version} = testData.deployConfig;
+            await blockchain.putKeyValue(target, '::rootDir', publicDirStorageId, version);
+            expect(publicDirStorageId).toMatch(hexRegExp);
+        },
+        TIMEOUTS.XL
+    );
 
-    it('Should add route to `routes.json` and deploy it', async () => {
-        expect.assertions(1);
-        const routesPath = path.join(__dirname, '..', 'resources', 'sample_site', 'routes.json');
-        const routesStr = await readFile(routesPath, 'utf-8');
-        const routesObj = JSON.parse(routesStr);
-        const newRoute = `/${publicDirStorageId}`;
-        routesObj[newRoute] = 'index.html';
-        const routesFileAsBuffer = Buffer.from(JSON.stringify(routesObj, null, 2));
-        routesStorageId = await storage.uploadFile(routesFileAsBuffer);
-        expect(routesStorageId).toMatch(hexRegExp);
-    }, 60000);
+    it(
+        'Should add route to `routes.json` and deploy it',
+        async () => {
+            expect.assertions(1);
+            const routesPath = path.join(
+                __dirname,
+                '..',
+                'resources',
+                'ynet_sample_site',
+                'routes.json'
+            );
+            const routesStr = await readFile(routesPath, 'utf-8');
+            const routesObj = JSON.parse(routesStr);
+            const newRoute = `/${publicDirStorageId}`;
+            routesObj[newRoute] = 'index.html';
+            const routesFileAsBuffer = Buffer.from(JSON.stringify(routesObj, null, 2));
+            routesStorageId = await storage.uploadFile(routesFileAsBuffer);
+            expect(routesStorageId).toMatch(hexRegExp);
+        },
+        TIMEOUTS.XL
+    );
 
-    it('Should add routes to "key-value storage"', async () => {
-        expect.assertions(1);
-        const {target, version} = testData.deployConfig;
-        const res = await blockchain.putZRecord(target, `0x${routesStorageId}`, version);
-        expect(res).toBe(undefined);
-    }, 60000);
+    it(
+        'Should add routes to "key-value storage"',
+        async () => {
+            expect.assertions(1);
+            const {target, version} = testData.deployConfig;
+            const res = await blockchain.putZRecord(target, `0x${routesStorageId}`, version);
+            expect(res).toBe(undefined);
+        },
+        TIMEOUTS.LG
+    );
 
-    it('Should deploy a contract', async () => {
-        expect.assertions(1);
+    it(
+        'Should deploy a contract',
+        async () => {
+            expect.assertions(1);
 
-        const artifacts = JSON.parse(
-            await readFile(
-                path.join(__dirname, '..', 'resources', 'sample_site', 'contracts', 'Why.json'),
-                'utf8'
-            )
-        ).contracts['Why.sol'].Why;
-        const contract = blockchain.getContractFromAbi(artifacts.abi);
-        const address = await blockchain.deployContract(
-            contract,
-            artifacts,
-            'Why'
-        );
+            const artifacts = JSON.parse(
+                await readFile(
+                    path.join(
+                        __dirname,
+                        '..',
+                        'resources',
+                        'ynet_sample_site',
+                        'contracts',
+                        'Why.json'
+                    ),
+                    'utf8'
+                )
+            ).contracts['Why.sol'].Why;
+            const contract = blockchain.getContractFromAbi(artifacts.abi);
+            const address = await blockchain.deployContract(contract, artifacts, 'Why');
 
-        const artifactsStorageId = await storage.uploadFile(JSON.stringify(artifacts));
+            const artifactsStorageId = await storage.uploadFile(JSON.stringify(artifacts));
 
-        await blockchain.putKeyValue(
-            'why',
-            'zweb/contracts/address/Why',
-            address,
-            'latest'
-        );
-        await blockchain.putKeyValue(
-            'why',
-            'zweb/contracts/abi/Why',
-            artifactsStorageId,
-            'latest'
-        );
+            const {target} = testData.deployConfig;
+            await blockchain.putKeyValue(target, 'zweb/contracts/address/Why', address, 'latest');
+            await blockchain.putKeyValue(
+                target,
+                'zweb/contracts/abi/Why',
+                artifactsStorageId,
+                'latest'
+            );
 
-        expect(address).toMatch(addressRegExp);
-    }, 60000);
+            expect(address).toMatch(addressRegExp);
+        },
+        TIMEOUTS.LG
+    );
 
-    it('Should fetch index.html making a GET request to the new domain', async () => {
-        expect.assertions(3);
-        await delay(5000);
+    it(
+        'Should fetch index.html making a GET request to the new domain',
+        async () => {
+            expect.assertions(3);
+            await delay(5000);
 
-        const res = await get(
-            `https://${testData.deployConfig.target}`,
-            {httpsAgent}
-        );
+            const res = await get(`https://${testData.deployConfig.target}`, {httpsAgent});
 
-        expect(res.status).toEqual(200);
-        expect(res.data).toMatch(/^<!DOCTYPE html>/);
-        expect(res.data).toMatch('<title>E2E Test Site</title>');
-    }, 20000);
+            expect(res.status).toEqual(200);
+            expect(res.data).toMatch(/^<!DOCTYPE html>/);
+            expect(res.data).toMatch('<title>E2E Test Site</title>');
+        },
+        TIMEOUTS.XL
+    );
 
-    it('Should fetch a file in the root folder', async () => {
-        expect.assertions(2);
+    it(
+        'Should fetch a file in the root folder',
+        async () => {
+            expect.assertions(2);
 
-        await delay(5000);
-        const res = await get(
-            `https://${testData.deployConfig.target}/index.css`,
-            {httpsAgent}
-        );
-        expect(res.status).toEqual(200);
-        expect(res.data).toMatch(/^h1 {/);
-    }, 20000);
+            await delay(5000);
+            const url = `https://${testData.deployConfig.target}/index.css`;
+            const res = await get(url, {httpsAgent});
+            expect(res.status).toEqual(200);
+            expect(res.data).toMatch(/^h1 {/);
+        },
+        TIMEOUTS.MD
+    );
 
-    it('Should return a file in a nested folder', async () => {
-        expect.assertions(2);
+    it(
+        'Should return a file in a nested folder',
+        async () => {
+            expect.assertions(2);
 
-        await delay(5000);
-        const res = await get(
-            `https://${testData.deployConfig.target}/images/sample.jpg`,
-            {httpsAgent}
-        );
-        expect(res.status).toEqual(200);
-        expect(res.headers['content-type']).toEqual('image/jpeg');
-    }, 20000);
+            await delay(5000);
+            const url = `https://${testData.deployConfig.target}/images/sample.jpg`;
+            const res = await get(url, {httpsAgent});
+            expect(res.status).toEqual(200);
+            expect(res.headers['content-type']).toEqual('image/jpeg');
+        },
+        TIMEOUTS.MD
+    );
 });
 
 describe('Proxy keyvalue', () => {
     // TODO: add test for storage upload
-    it('Should append keyvalue', async () => {
-        expect.assertions(2);
+    it(
+        'Should append keyvalue',
+        async () => {
+            expect.assertions(2);
 
-        await delay(5000);
-        const res = await post(
-            `https://${testData.deployConfig.target}/_keyvalue_append/foo`,
-            'foo=bar&baz=123',
-            {httpsAgent}
-        );
+            await delay(5000);
+            const res = await post(
+                `https://${testData.deployConfig.target}/_keyvalue_append/foo`,
+                'foo=bar&baz=123',
+                {httpsAgent}
+            );
 
-        expect(res.status).toEqual(200);
-        expect(res.data).toEqual('Success');
-    }, 10000);
+            expect(res.status).toEqual(200);
+            expect(res.data).toEqual('Success');
+        },
+        TIMEOUTS.XS
+    );
 
-    it('Should get keyvalue', async () => {
-        expect.assertions(3);
+    it(
+        'Should get keyvalue',
+        async () => {
+            expect.assertions(3);
 
-        await delay(5000);
-        const res = await get(
-            `https://${testData.deployConfig.target}/_keyvalue_get/foo0`,
-            {httpsAgent}
-        );
+            await delay(5000);
+            const url = `https://${testData.deployConfig.target}/_keyvalue_get/foo0`;
+            const res = await get(url, {httpsAgent});
 
-        expect(res.status).toEqual(200);
-        expect(res.data.foo).toEqual('bar');
-        // TODO: this is the problem with x-www-form-urlencoded: numbers are not parsed correctly
-        expect(res.data.baz).toEqual('123');
-    }, 10000);
+            expect(res.status).toEqual(200);
+            expect(res.data.foo).toEqual('bar');
+            // TODO: this is the problem with x-www-form-urlencoded: numbers are not parsed correctly
+            expect(res.data.baz).toEqual('123');
+        },
+        TIMEOUTS.XS
+    );
 
-    it('Should return null for non-existing keyvalue', async () => {
-        expect.assertions(2);
+    it(
+        'Should return null for non-existing keyvalue',
+        async () => {
+            expect.assertions(2);
 
-        const res = await get(
-            `https://${testData.deployConfig.target}/_keyvalue_get/notexists`,
-            {httpsAgent}
-        );
+            const url = `https://${testData.deployConfig.target}/_keyvalue_get/notexists`;
+            const res = await get(url, {httpsAgent});
 
-        expect(res.status).toEqual(200);
-        expect(res.data).toEqual(null);
-    }, 10000);
+            expect(res.status).toEqual(200);
+            expect(res.data).toEqual(null);
+        },
+        TIMEOUTS.XS
+    );
 });
 
 describe('Proxy contract send', () => {
@@ -215,46 +260,58 @@ describe('Proxy contract send', () => {
     // TODO: contract send for point host seems not possible, bc host argument is passed incorrectly
     // is it a bug or is it intended?
 
-    it('Should store value in why contract', async () => {
-        expect.assertions(1);
+    it(
+        'Should store value in why contract',
+        async () => {
+            expect.assertions(1);
 
-        await delay(5000);
-        const res = await post(
-            `https://${testData.deployConfig.target}/_contract_send/Why.storeValue(value)`,
-            'value=foo',
-            {httpsAgent}
-        );
+            await delay(5000);
+            const res = await post(
+                `https://${testData.deployConfig.target}/_contract_send/Why.storeValue(value)`,
+                'value=foo',
+                {httpsAgent}
+            );
 
-        expect(res.status).toEqual(200);
-    }, 10000);
+            expect(res.status).toEqual(200);
+        },
+        TIMEOUTS.XS
+    );
 
     // Function without args call
     // TODO: this is useless, returns nothing.
-    it('Should get values in why contract', async () => {
-        expect.assertions(1);
+    it(
+        'Should get values in why contract',
+        async () => {
+            expect.assertions(1);
 
-        await delay(5000);
-        const res = await post(
-            `https://${testData.deployConfig.target}/_contract_send/Why.getValues()`,
-            '',
-            {httpsAgent}
-        );
+            await delay(5000);
+            const res = await post(
+                `https://${testData.deployConfig.target}/_contract_send/Why.getValues()`,
+                '',
+                {httpsAgent}
+            );
 
-        expect(res.status).toEqual(200);
-    }, 10000);
+            expect(res.status).toEqual(200);
+        },
+        TIMEOUTS.XS
+    );
 
     // Function with args call
     // TODO: this is useless, returns nothing.
-    it('Should get stored value in why contract', async () => {
-        expect.assertions(1);
+    it(
+        'Should get stored value in why contract',
+        async () => {
+            expect.assertions(1);
 
-        await delay(5000);
-        const res = await post(
-            `https://${testData.deployConfig.target}/_contract_send/Why.getValue(id)`,
-            'id=0',
-            {httpsAgent}
-        );
+            await delay(5000);
+            const res = await post(
+                `https://${testData.deployConfig.target}/_contract_send/Why.getValue(id)`,
+                'id=0',
+                {httpsAgent}
+            );
 
-        expect(res.status).toEqual(200);
-    }, 10000);
+            expect(res.status).toEqual(200);
+        },
+        TIMEOUTS.XS
+    );
 });
--- tests/docker/proxy-api.spec.js
+++ tests/docker/proxy-api.spec.js
@@ -1,5 +1,6 @@
 import {get, post} from 'axios';
 import HttpsAgent from 'https-proxy-agent';
+import {TIMEOUTS} from '../timeouts';
 
 const DOCKER_POINT_NODE = 'point_node';
 const POINT_NODE = process.env.TEST_POINT_NODE || DOCKER_POINT_NODE;
@@ -15,47 +16,53 @@ const httpsAgent = new HttpsAgent(httpAgentCfg);
 describe('API requests through proxy', () => {
     // TODO: check cases for malformed body and other inappropriate requests
     // (but this depends on API, not proxy - the latter just forwards response with its status)
-    it('API GET: should return meta info', async () => {
-        expect.assertions(3);
+    it(
+        'API GET: should return meta info',
+        async () => {
+            expect.assertions(3);
 
-        const res = await get(
-            'https://blog.z/v1/api/status/meta',
-            {httpsAgent}
-        );
+            const res = await get('https://blog.z/v1/api/status/meta', {httpsAgent});
 
-        expect(res.status).toEqual(200);
-        expect(res.data.data.nodeJsVersion).toBeTruthy();
-        expect(res.data.data.pointNodeVersion).toBeTruthy();
-    }, 10000);
+            expect(res.status).toEqual(200);
+            expect(res.data.data.nodeJsVersion).toBeTruthy();
+            expect(res.data.data.pointNodeVersion).toBeTruthy();
+        },
+        TIMEOUTS.XS
+    );
 
-    it('Should return 404 for non-existing GET request', async () => {
-        expect.assertions(1);
+    it(
+        'Should return 404 for non-existing GET request',
+        async () => {
+            expect.assertions(1);
 
-        const res = await get(
-            'https://blog.z/v1/api/something/that/not/exists',
-            {
+            const res = await get('https://blog.z/v1/api/something/that/not/exists', {
                 httpsAgent,
                 validateStatus: () => true
-            }
-        );
+            });
 
-        expect(res.status).toEqual(404);
-    }, 10000);
+            expect(res.status).toEqual(404);
+        },
+        TIMEOUTS.XS
+    );
 
-    it('Should return 404 for non-existing POST request', async () => {
-        expect.assertions(1);
+    it(
+        'Should return 404 for non-existing POST request',
+        async () => {
+            expect.assertions(1);
 
-        const res = await post(
-            'https://blog.z/v1/api/something/that/not/exists',
-            {},
-            {
-                httpsAgent,
-                validateStatus: () => true
-            }
-        );
+            const res = await post(
+                'https://blog.z/v1/api/something/that/not/exists',
+                {},
+                {
+                    httpsAgent,
+                    validateStatus: () => true
+                }
+            );
 
-        expect(res.status).toEqual(404);
-    }, 10000);
+            expect(res.status).toEqual(404);
+        },
+        TIMEOUTS.XS
+    );
 
     // TODO: this is broken on API side
     // it('API POST: should make a contract call', async () => {
