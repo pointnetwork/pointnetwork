68f30dcef780122977ba71d765f9b10c12ac1281
Koochr <koochr@protonmail.com>
2022-06-15 02:41:16 +0100

    Fix double sending ws message

--- src/client/proxy/handlers/common.ts
+++ src/client/proxy/handlers/common.ts
@@ -401,7 +401,7 @@ const getHttpRequestHandler = (ctx: any) => async (req: FastifyRequest, res: Fas
 const attachCommonHandler = (server: FastifyInstance, ctx: any) => {
     const handler = getHttpRequestHandler(ctx);
     const wsHandler = ({socket}: SocketStream, {hostname}: FastifyRequest) =>
-        void new ZProxySocketController(ctx, socket, server.websocketServer, hostname).init();
+        void new ZProxySocketController(ctx, socket, server.websocketServer, hostname);
 
     // Handle websocket requests.
     server.route({
