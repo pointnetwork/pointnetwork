d2067a53f246913c1746ff28ea388bc51b35e84f
Koochr <koochr@protonmail.com>
2022-06-21 15:38:03 +0100

    Move privileged methods from API

--- src/api/controllers/WalletController.js
+++ src/api/controllers/WalletController.js
@@ -39,6 +39,11 @@ class WalletController extends PointSDKController {
     }
 
     hash() {
+        const {host} = this.req.headers;
+        if (host !== 'confirmation-window') {
+            return {status: 403};
+        }
+
         const partialPK = getNetworkPrivateKey().slice(0, 33);
         const hashBuffer = ethereumjs.sha256(Buffer.from(partialPK));
         const hash = ethereumjs.bufferToHex(hashBuffer);
@@ -132,11 +137,20 @@ class WalletController extends PointSDKController {
     }
 
     async send() {
+        const {host} = this.req.headers;
+        if (host !== 'point') {
+            return {status: 403};
+        }
         const {to, network, value, messageId} = this.payload;
         return sendTransaction({to, network, value, messageId});
     }
 
     async sendToken() {
+        const {host} = this.req.headers;
+        if (host !== 'point') {
+            return {status: 403};
+        }
+
         const {tokenAddress, to, network, value, messageId} = this.payload;
 
         return sendToken({tokenAddress, to, network, value, messageId});
--- src/client/proxy/handlers/index.ts
+++ src/client/proxy/handlers/index.ts
@@ -4,11 +4,13 @@ import attachStorageHandlers from './storage';
 import attachKeyValueHanlders from './keyValue';
 import attachContractSendHandler from './contractSend';
 import attachApiHandler from './api';
+import attachPointApiHandler from './pointApi';
 
 // TODO: ctx is needed for Renderer and keyvalue, remove it later
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const attachHandlers = (server: FastifyInstance, ctx: any) => {
     attachStorageHandlers(server);
+    attachPointApiHandler(server);
     attachApiHandler(server);
     attachContractSendHandler(server);
     attachKeyValueHanlders(server);
