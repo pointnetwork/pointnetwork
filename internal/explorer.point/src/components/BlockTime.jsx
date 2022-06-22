9bd8c35fd6642b472581cdb20b1957ce2c5cad4e
Darren Jensen <darren.jensen@gmail.com>
2022-06-22 15:06:46 +0700

    revert explorer for develop

--- internal/explorer.point/src/components/BlockTime.jsx
+++ internal/explorer.point/src/components/BlockTime.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import Loading from '../components/Loading';
-import * as dayjs from 'dayjs';
 
 const BlockTime = ({ blockNumber }) => {
     const [isLoading, setIsLoading] = useState(true);
@@ -25,11 +24,7 @@ const BlockTime = ({ blockNumber }) => {
         setIsLoading(false);
     };
 
-    return isLoading ? (
-        <Loading />
-    ) : (
-        dayjs(timestamp * 1000).format('YYYY-MM-DD H:m:s')
-    );
+    return isLoading ? <Loading /> : new Date(timestamp * 1000).toUTCString();
 };
 
 export default BlockTime;
