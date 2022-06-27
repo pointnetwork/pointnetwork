0050d87671ab6b797574ecff47567255b4a3abef
Koochr <koochr@protonmail.com>
2022-06-27 15:28:55 +0100

    Add tests for key value

--- src/client/proxy/handlers/index.ts
+++ src/client/proxy/handlers/index.ts
@@ -1,19 +1,19 @@
 import {FastifyInstance} from 'fastify';
 import attachCommonHandler from './common';
 import attachStorageHandlers from './storage';
-import attachKeyValueHanlders from './keyValue';
+import attachKeyValueHandlers from './keyValue';
 import attachContractSendHandler from './contractSend';
 import attachApiHandler from './api';
 import attachPointApiHandler from './pointApi';
 
-// TODO: ctx is needed for Renderer and keyvalue, remove it later
+// TODO: ctx is needed for Renderer, remove it later
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const attachHandlers = (server: FastifyInstance, ctx: any) => {
     attachStorageHandlers(server);
     attachPointApiHandler(server);
     attachApiHandler(server);
     attachContractSendHandler(server);
-    attachKeyValueHanlders(server);
+    attachKeyValueHandlers(server);
     attachCommonHandler(server, ctx);
 };
 
--- tests/client/proxy.spec.js
+++ tests/client/proxy.spec.js
@@ -1,12 +1,23 @@
 import attachHandlers from '../../src/client/proxy/handlers';
+import httpsServer from '../../src/client/proxy/httpsServer';
+import ethereum from '../../src/network/providers/ethereum';
 
 jest.mock('../../src/network/providers/ethereum', () => ({
     __esModule: true,
-    default: {sendToContract: jest.fn(async () => 'OK')}
+    default: {
+        sendToContract: jest.fn(async () => 'test_send_to_contract_reply'),
+        getKeyValue: jest.fn(async (identity, key) => {
+            // If we return something unconditionally, we'll run into infinite loop
+            // when calling keyValue.list in keyvalue_append
+            if (key.endsWith('0')) {
+                return 'test_value';
+            }
+            return null;
+        }),
+        putKeyValue: jest.fn(async () => {}),
+        getKeyLastVersion: jest.fn(async () => '1.0')
+    }
 }));
-
-import httpsServer from '../../src/client/proxy/httpsServer';
-import ethereum from '../../src/network/providers/ethereum';
 
 beforeAll(() => {
     attachHandlers(httpsServer, {});
@@ -18,18 +29,18 @@ describe('Proxy', () => {
 
         const res = await httpsServer.inject({
             method: 'POST',
-            url: 'https://somedomain.point/_contract_send/ContractName.funcName(value1, value2)',
+            url: 'https://contract_send_domain.point/_contract_send/ContractName.funcName(value1, value2)',
             payload: 'value1=foo&value2=bar',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'host': 'somedomain.point'
+                'host': 'contract_send_domain.point'
             }
         });
 
         expect(res.statusCode).toEqual(200);
-        expect(res.statusMessage).toEqual('OK');
+        expect(res.payload).toEqual('test_send_to_contract_reply');
         expect(ethereum.sendToContract).toHaveBeenCalledWith(
-            'somedomain',
+            'contract_send_domain',
             'ContractName',
             'funcName',
             ['foo', 'bar']
@@ -48,5 +59,44 @@ describe('Proxy', () => {
         expect(res.statusCode).toEqual(200);
         expect(res.payload).toMatch(/^<!DOCTYPE html>/);
         expect(res.payload).toMatch('<title>Point Explorer</title>');
+    });
+
+    it('Should perform keyvalue_get request', async () => {
+        expect.assertions(3);
+
+        const res = await httpsServer.inject({
+            method: 'GET',
+            url: 'https://keyvalue_get_domain.point/_keyvalue_get/test_key0',
+            headers: {host: 'keyvalue_get_domain.point'}
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.payload).toEqual('test_value');
+        expect(ethereum.getKeyValue).toHaveBeenCalledWith('keyvalue_get_domain', 'test_key0');
+    });
+
+    it('Should perform keyvalue_append request', async () => {
+        expect.assertions(3);
+
+        const res = await httpsServer.inject({
+            method: 'POST',
+            url: 'https://keyvalue_append_domain.point/_keyvalue_append/test_key',
+            payload: 'value1=foo&value2=bar',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'host': 'keyvalue_append_domain.point'
+            }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(ethereum.getKeyLastVersion).toHaveBeenCalledWith('keyvalue_append_domain', '::rootDir');
+        expect(ethereum.putKeyValue).toHaveBeenCalledWith(
+            'keyvalue_append_domain',
+            // Our mocked getKeyValue will return value for test_key0, so test_key1 should be
+            // the one to be appended
+            'test_key1',
+            expect.stringMatching(/^\{"value1":"foo","value2":"bar",/),
+            '1.0'
+        );
     });
 });
