f356ae22730702b7fa29402a90984e7232676a8d
Koochr <koochr@protonmail.com>
2022-06-27 01:56:11 +0100

    Add publishing tests, omit global setup when not needed

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,16 +1,20 @@
-const {getContractAddress} = require('../dist/util');
-const initFolders = require('../dist/initFolders');
-const storage = require('../dist/client/storage');
-const migrate = require('../dist/util/migrate');
+if (process.env.TESTING_IN_DOCKER) {
+    const {getContractAddress} = require('../dist/util');
+    const initFolders = require('../dist/initFolders');
+    const storage = require('../dist/client/storage');
+    const migrate = require('../dist/util/migrate');
 
-module.exports = async () => {
-    const identityContractAddress = getContractAddress('Identity');
-    if (!identityContractAddress) {
-        throw new Error('Could not get Identity contract address');
-    }
-    process.env.IDENTITY_CONTRACT_ADDRESS = identityContractAddress;
+    module.exports = async () => {
+        const identityContractAddress = getContractAddress('Identity');
+        if (!identityContractAddress) {
+            throw new Error('Could not get Identity contract address');
+        }
+        process.env.IDENTITY_CONTRACT_ADDRESS = identityContractAddress;
 
-    await initFolders.default();
-    await migrate.default();
-    await storage.init();
-};
+        await initFolders.default();
+        await migrate.default();
+        await storage.init();
+    };
+} else {
+    module.exports = () => {};
+}
