fc3fe0fa30960883cfc804545eeed06e57b45988
Koochr <koochr@protonmail.com>
2022-06-27 02:06:36 +0100

    Rollback global setup, add key to test ci env

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,20 +1,16 @@
-if (process.env.TESTING_IN_DOCKER) {
-    const {getContractAddress} = require('../dist/util');
-    const initFolders = require('../dist/initFolders');
-    const storage = require('../dist/client/storage');
-    const migrate = require('../dist/util/migrate');
+const {getContractAddress} = require('../dist/util');
+const initFolders = require('../dist/initFolders');
+const storage = require('../dist/client/storage');
+const migrate = require('../dist/util/migrate');
 
-    module.exports = async () => {
-        const identityContractAddress = getContractAddress('Identity');
-        if (!identityContractAddress) {
-            throw new Error('Could not get Identity contract address');
-        }
-        process.env.IDENTITY_CONTRACT_ADDRESS = identityContractAddress;
+module.exports = async () => {
+    const identityContractAddress = getContractAddress('Identity');
+    if (!identityContractAddress) {
+        throw new Error('Could not get Identity contract address');
+    }
+    process.env.IDENTITY_CONTRACT_ADDRESS = identityContractAddress;
 
-        await initFolders.default();
-        await migrate.default();
-        await storage.init();
-    };
-} else {
-    module.exports = () => {};
-}
+    await initFolders.default();
+    await migrate.default();
+    await storage.init();
+};
