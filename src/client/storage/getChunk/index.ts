c642ef5f4e33c5a91178e5223c51285d73386c37
Koochr <koochr@protonmail.com>
2022-07-01 16:05:48 +0100

    Change folder names in storage

--- src/client/storage/getChunk/index.ts
+++
@@ -1,7 +0,0 @@
-import getChunk_normal from './getChunk';
-import getChunk_arlocal from './getChunk_arlocal';
-import {MODE} from '../config';
-
-const getChunk = MODE === 'zappdev' ? getChunk_arlocal : getChunk_normal;
-
-export default getChunk;
