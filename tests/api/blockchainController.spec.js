39255c87d6b57adbdec05560ff67460ae036ed9a
Koochr <koochr@protonmail.com>
2022-07-01 17:18:50 +0100

    Add test for blockchain controller

---
+++ tests/api/blockchainController.spec.js
@@ -0,0 +1,78 @@
+import apiServer from '../../src/api/server';
+import handleRPC from '../../src/rpc/rpc-handlers';
+import config from 'config';
+
+jest.mock('../../src/rpc/rpc-handlers', () => ({
+    __esModule: true,
+    default: jest.fn(async () => ({
+        status: 200,
+        result: 'mock_rpc_res'
+    }))
+}));
+
+describe('API blockchain controller', () => {
+    it('Should return 401 with API key missing', async () => {
+        expect.assertions(2);
+
+        const call = {
+            id: new Date().getTime(),
+            method: 'eth_call',
+            params: [{
+                from: '0xF6690149C78D0254EF65FDAA6B23EC6A342f6d8D',
+                to: '0xa2694005d321212F340c8422FAAcd1dfa5450A56',
+                data: '0x'
+            }],
+            network: 'ynet'
+        };
+
+        const res = await apiServer.inject({
+            method: 'POST',
+            url: 'https://blockchain_test_domain.point/v1/api/blockchain',
+            payload: JSON.stringify(call),
+            headers: {
+                'Content-Type': 'application/json',
+                'host': 'blockchain_test_domain.point',
+                'origin': 'https://blockchain_test_domain.point'
+            }
+        });
+
+        expect(res.statusCode).toEqual(401);
+        expect(handleRPC).not.toHaveBeenCalled();
+    });
+
+    it('RPC Request', async () => {
+        expect.assertions(3);
+
+        const SDK_AUTH_KEY = config.get('api.sdk_auth_key');
+
+        const call = {
+            id: new Date().getTime(),
+            method: 'eth_call',
+            params: [{
+                from: '0xF6690149C78D0254EF65FDAA6B23EC6A342f6d8D',
+                to: '0xa2694005d321212F340c8422FAAcd1dfa5450A56',
+                data: '0x'
+            }],
+            network: 'ynet'
+        };
+
+        const res = await apiServer.inject({
+            method: 'POST',
+            url: 'https://blockchain_test_domain.point/v1/api/blockchain',
+            payload: JSON.stringify(call),
+            headers: {
+                'Content-Type': 'application/json',
+                'host': 'blockchain_test_domain.point',
+                'origin': 'https://blockchain_test_domain.point',
+                'authorization': `Bearer ${SDK_AUTH_KEY}`
+            }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.payload).toEqual('mock_rpc_res');
+        expect(handleRPC).toHaveBeenCalledWith({
+            ...call,
+            origin: 'https://blockchain_test_domain.point'
+        });
+    });
+});
