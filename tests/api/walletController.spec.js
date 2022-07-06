b0a68bb6dc03d70321c0db0d80ecf6b61a2248fc
Koochr <koochr@protonmail.com>
2022-07-06 15:38:26 +0100

    Try node v14

--- tests/api/walletController.spec.js
+++ tests/api/walletController.spec.js
@@ -17,4 +17,38 @@ describe('Wallet controller', () => {
             headers: {}
         });
     });
+
+    it('Eth Address', async () => {
+        expect.assertions(2);
+
+        const res = await apiServer.inject({
+            method: 'GET',
+            url: 'https://pk.point/v1/api/wallet/address',
+            headers: {host: 'pk.point'}
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(JSON.parse(res.payload)).toEqual({
+            status: 200,
+            data: {address: expect.stringMatching(/^0x/)},
+            headers: {}
+        });
+    });
+
+    it('Sol Address', async () => {
+        expect.assertions(2);
+
+        const res = await apiServer.inject({
+            method: 'GET',
+            url: 'https://pk.point/v1/api/wallet/address?network=solana_devnet',
+            headers: {host: 'pk.point'}
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(JSON.parse(res.payload)).toEqual({
+            status: 200,
+            data: {address: expect.stringMatching(/^[a-zA-Z\d]{44}$/)},
+            headers: {}
+        });
+    });
 });
