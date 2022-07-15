1dd5df3e1cfc77434ac82c0996565e52b4575952
Diogo S. Mendonca <diogosmendonca@gmail.com>
2022-07-15 11:26:07 -0300

    fix/improve web2 redirect url

--- src/api/controllers/Web2Controller.js
+++ src/api/controllers/Web2Controller.js
@@ -6,15 +6,11 @@ class Web2Controller extends PointSDKController {
         super(req, true);
         this.req = req;
         this.host = this.req.headers.host;
-
-        if (!this.host === 'point') return reply.callNotFound();
-
         this.payload = req.body;
         this.reply = reply;
     }
 
     async open() {
-        if (!this.host === 'point') return reply.callNotFound();
         const url = this.payload.urlToOpen;
         try {
             open(url);
