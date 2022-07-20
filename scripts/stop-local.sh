853e61ae530fb662879cfb0afaa0e26ddf0a26eb
Darren Jensen <darren.jensen@gmail.com>
2022-07-20 17:52:53 +0700

    remove term grep from kill command to avoid missing pid error

--- scripts/stop-local.sh
+++ scripts/stop-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Sending kill signal to ganache and arlocal processes"
 
-kill $(ps aux | grep 'ganache' | awk '{print $2}')
-kill $(ps aux | grep 'arlocal' | awk '{print $2}')
+kill $(ps aux | grep 'ganache' | grep -v 'grep' | awk '{print $2}')
+kill $(ps aux | grep 'arlocal' | grep -v 'grep' | awk '{print $2}')
 
 echo "Finished"
