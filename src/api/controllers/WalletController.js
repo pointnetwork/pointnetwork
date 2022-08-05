fd1c7c28b41adedd760e025f32d20623db90a552
German Viescas <german@pointlabs.org>
2022-08-05 11:17:25 -0300

    Optionally set gas price in config

--- src/api/controllers/WalletController.js
+++ src/api/controllers/WalletController.js
@@ -21,7 +21,7 @@ const timeout = (prom, time, exception) => {
     let timer;
     return Promise.race([
         prom,
-        new Promise((_r, rej) => (timer = setTimeout(rej, time, exception)))
+        new Promise((_r, rej) => timer = setTimeout(rej, time, exception))
     ]).finally(() => clearTimeout(timer));
 };
 
