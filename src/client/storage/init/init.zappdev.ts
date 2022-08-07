1c1aeb395766cd82d0b342961f0f9be6e02263b5
Serge Var <serge@pointnetwork.io>
2022-08-07 18:40:00 +0300

    Only try to mint AR tokens if config.storage.use_arlocal is set to true

--- src/client/storage/init/init.zappdev.ts
+++ src/client/storage/init/init.zappdev.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import {HOST, PORT, PROTOCOL} from '../config';
+import {HOST, PORT, PROTOCOL, USE_ARLOCAL} from '../config';
 import arweave, {arweaveKey} from '../client/client_zappdev';
 
 const init = async () => {
     // mint tokens on arlocal
-    if (arweaveKey !== undefined) {
+    if (USE_ARLOCAL && arweaveKey !== undefined) {
         const address = await arweave.wallets.jwkToAddress(arweaveKey);
         await axios.get(`${PROTOCOL}://${HOST}:${PORT}/mint/${address}/100000000000000000000`);
     }
