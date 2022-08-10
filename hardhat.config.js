c558703f402a140532e0ea3164a15732a0aa9304
Koochr <koochr@protonmail.com>
2022-08-10 21:16:23 +0100

    Fix tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('@openzeppelin/hardhat-upgrades');
 require('./hardhat/tasks/explorer/explorer-set-index-md');
 
 let ynetPrivateKey = process.env.DEPLOYER_ACCOUNT;
-if (ynetPrivateKey === undefined && process.env.MODE !== 'e2e' && process.env.MODE !== 'zappdev'){
+if (!['zappdev', 'e2e', 'test'].includes(process.env.MODE) && ynetPrivateKey === undefined) {
     const homedir = require('os').homedir();
     require('path').resolve(homedir, '.point', 'keystore', 'key.json');
     const wallet = require('ethereumjs-wallet').hdkey.fromMasterSeed(
@@ -13,10 +13,9 @@ if (ynetPrivateKey === undefined && process.env.MODE !== 'e2e' && process.env.MO
         )
     ).getWallet();
     ynetPrivateKey = wallet.getPrivateKey().toString('hex');
-}
-
-if (process.env.MODE !== 'e2e' && process.env.MODE !== 'zappdev' && ynetPrivateKey === undefined){
-    throw new Error('ynetPrivateKey is not set.');
+    if (!ynetPrivateKey) {
+        throw new Error('ynetPrivateKey is not set.');
+    }
 }
 
 const privateKey = process.env.DEPLOYER_ACCOUNT || '0x011967d88c6b79116bb879d4c2bc2c3caa23569edd85dfe0bc596846837bbc8e';
@@ -63,7 +62,7 @@ const config = {
                 version: '0.8.7',
                 settings: {...optimizerConfig}
             }
-        ],
+        ]
     },
     paths: {
         artifacts:'./hardhat/build',
