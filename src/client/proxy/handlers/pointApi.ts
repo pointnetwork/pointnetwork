f93e5bf6ae8a8e97877bf4fe1403094e1909a3a1
German Viescas <german@pointlabs.org>
2022-08-17 15:56:25 -0300

    Comment out deploy route

--- src/client/proxy/handlers/pointApi.ts
+++ src/client/proxy/handlers/pointApi.ts
@@ -1,41 +1,41 @@
 import {FastifyInstance, FastifyRequest} from 'fastify';
-import DeployController from '../../../api/controllers/DeployController';
+// import DeployController from '../../../api/controllers/DeployController';
 import WalletController from '../../../api/controllers/WalletController';
 
 const attachPointApiHandler = (server: FastifyInstance) => {
     server.route({
         method: ['GET', 'POST'],
         url: '/point_api/wallet/:method',
-        handler: async (req: FastifyRequest<{ Params: {'method': string}; }>, res) => {
+        handler: async (req: FastifyRequest<{Params: {method: string}}>, res) => {
             const controller = new WalletController(req, res);
 
             return controller[req.params.method as 'send']();
         }
     });
 
-    server.route({
-        method: ['POST'],
-        url: '/point_api/deploy',
-        handler: async (req, res) => {
-            const {host} = req.headers;
-            if (host !== 'point') {
-                res.status(403).send('Forbidden');
-            }
-
-            const controller = new DeployController(req);
-
-            const {status, error} = await controller.deploy();
-            
-            res.status(status === 'success'
-                ? 200
-                : error === 'deploy path not specified'
-                    ? 400
-                    : 500).send({
-                status,
-                error
-            });
-        }
-    });
+    // server.route({
+    //     method: ['POST'],
+    //     url: '/point_api/deploy',
+    //     handler: async (req, res) => {
+    //         const {host} = req.headers;
+    //         if (host !== 'point') {
+    //             res.status(403).send('Forbidden');
+    //         }
+    //
+    //         const controller = new DeployController(req);
+    //
+    //         const {status, error} = await controller.deploy();
+    //
+    //         res.status(status === 'success'
+    //             ? 200
+    //             : error === 'deploy path not specified'
+    //                 ? 400
+    //                 : 500).send({
+    //             status,
+    //             error
+    //         });
+    //     }
+    // });
 };
 
 export default attachPointApiHandler;
