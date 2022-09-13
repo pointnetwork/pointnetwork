c2f920374e0742c87a79b58bdfb9f5c71d2597ad
German Viescas <german@pointlabs.org>
2022-09-13 15:57:31 -0300

    Make resolveDomain endpoint public

--- src/api/api_routes.js
+++ src/api/api_routes.js
@@ -32,8 +32,18 @@ module.exports = [
     ['POST', '/v1/api/identity/blockTimestamp', 'IdentityController@blockTimestamp'], // TODO: why POST?
     ['POST', '/v1/api/identity/open', 'IdentityController@openLink', {protected: true}],
     ['POST', '/v1/api/identity/register', 'IdentityController@registerIdentity', {protected: true}],
-    ['POST', '/v1/api/identity/sub/register', 'IdentityController@registerSubIdentity', {protected: true}],
-    ['GET', '/v1/api/identity/resolve/:domain', 'IdentityController@resolveDomain', {protected: true}],
+    [
+        'POST',
+        '/v1/api/identity/sub/register',
+        'IdentityController@registerSubIdentity',
+        {protected: true}
+    ],
+    [
+        'GET',
+        '/v1/api/identity/resolve/:domain',
+        'IdentityController@resolveDomain',
+        {protected: false}
+    ],
     ['POST', '/v1/api/identity/ikvPut', 'IdentityController@ikvPut', {protected: true}],
     ['GET', '/v1/api/status/ping', 'PingController@ping'],
     ['GET', '/v1/api/status/meta', 'StatusController@meta'],
@@ -50,10 +60,20 @@ module.exports = [
     ['GET', '/v1/api/wallet/balance', 'WalletController@balance', {protected: true}],
     ['GET', '/v1/api/wallet/address', 'WalletController@address', {protected: true}],
     ['GET', '/v1/api/wallet/getWalletInfo', 'WalletController@getWalletInfo', {protected: true}],
-    ['GET', '/v1/api/wallet/getTokenBalances', 'WalletController@getTokenBalances', {protected: true}],
+    [
+        'GET',
+        '/v1/api/wallet/getTokenBalances',
+        'WalletController@getTokenBalances',
+        {protected: true}
+    ],
     ['POST', '/v1/api/wallet/encryptData', 'WalletController@encryptData', {protected: true}],
     ['POST', '/v1/api/wallet/decryptData', 'WalletController@decryptData', {protected: true}],
-    ['POST', '/v1/api/wallet/decryptSymmetricKey', 'WalletController@decryptSymmetricKey', {protected: true}],
+    [
+        'POST',
+        '/v1/api/wallet/decryptSymmetricKey',
+        'WalletController@decryptSymmetricKey',
+        {protected: true}
+    ],
     [
         'POST',
         '/v1/api/wallet/decryptDataWithDecryptedKey',
