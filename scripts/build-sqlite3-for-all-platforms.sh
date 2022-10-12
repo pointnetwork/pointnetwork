ed45690bab342bb4f17db0670939fbfe09250df5
Denis Voropaev <d.n.voropaev@gmail.com>
2022-10-12 16:49:25 +0300

    Revert "add linux to sqlite3 build"
    This reverts commit ff4b091aa1dde5b68c081bc1f9c710b39c9595da.

--- scripts/build-sqlite3-for-all-platforms.sh
+++ scripts/build-sqlite3-for-all-platforms.sh
@@ -3,7 +3,7 @@
 # This script is used to build sqlite3 binaries to be packaged by vercel/pkg
 # You can add more platforms and archs in the array below
 
-platforms=(darwin win32 linux);
+platforms=(darwin win32);
 archs=(x64);
 libcs=(glibc musl);
 
