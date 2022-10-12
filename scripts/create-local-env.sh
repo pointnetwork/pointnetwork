aae5a11cd370a6af458089b31bb8105715eebf5a
muhammadp <muhammad@pointlabs.org>
2022-10-12 17:42:35 -0400

    Update create-local-env.sh

--- scripts/create-local-env.sh
+++ scripts/create-local-env.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+export NODE_PATH=$(which node)/bin
+export PATH=$NODE_PATH:$PATH
+
+PLATFORM=''
+
+case "$OSTYPE" in
+  darwin*)  PLATFORM="darwin" ;;
+  linux*)   PLATFORM="linux" ;;
+  msys*)    PLATFORM="win32" ;;
+  cygwin*)  PLATFORM="win32" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
 isNpmPackageInstalled() {
   npm list -g | grep -c $1 > /dev/null 2>&1
 }
@@ -20,8 +33,8 @@ do
       echo $package is already installed, skipping installation.
   else
       echo $package is NOT installed, installing...
-      echo npm i -g $package
-      npm i -g $package
+      echo npm install --target_platform=$PLATFORM -g $package
+      npm install --target_platform=$PLATFORM -g $package
   fi
 done
 
@@ -29,7 +42,6 @@ done
 echo "installing identity contract project"
 source .bash_alias
 
-#checking it point-contracts folder exists
 if [ ! -d "../point-contracts" ]; then
   echo "point-contracts repository does not exist, cloning"
   cd ..
@@ -42,16 +54,24 @@ cd ../point-contracts
 rm -rf cache
 rm -rf typechain
 rm -rf build
-echo "npm i"
-npm i
+echo "npm install --target_platform=$PLATFORM"
+npm install --target_platform=$PLATFORM
 echo "npx hardhat compile"
 npx hardhat compile
 cd ../pointnetwork
+npm install --target_platform=$PLATFORM
+
+cd internal/explorer.point
+npm install --target_platform=$PLATFORM
+
+cd ../../../pointnetwork
 
 #creating local directories
 echo "creating local directories"
 echo "mkdir -p ~/workspace/pn/devlocal/keystore"
 mkdir -p ~/workspace/pn/devlocal/keystore
+echo "mkdir -p ~/workspace/pn/devlocal/contracts"
+mkdir -p ~/workspace/pn/devlocal/contracts
 
 echo "cp resources/blockchain-test-key.json ~/workspace/pn/devlocal/keystore/key.json"
 cp resources/blockchain-test-key.json ~/workspace/pn/devlocal/keystore/key.json
@@ -59,11 +79,11 @@ echo "cp resources/arweave-test-key.json ~/workspace/pn/devlocal/keystore/arweav
 cp resources/arweave-test-key.json ~/workspace/pn/devlocal/keystore/arweave.json
 echo 'Creating token in ~/workspace/pn/devlocal/keystore/token.txt'
 echo 'xGrqHMNXLhjEubp1soD0hHN6nXIBsUwA' > ~/workspace/pn/devlocal/keystore/token.txt
-echo "cp hardhat/build/contracts/Identity.sol/Identity.json ~/workspace/pn/devlocal/contracts/Identity.json"
-cp hardhat/build/contracts/Identity.sol/Identity.json ~/workspace/pn/devlocal/contracts/Identity.json
+echo "cp ../point-contracts/build/contracts/Identity.sol/Identity.json ~/workspace/pn/devlocal/contracts/Identity.json"
+cp ../point-contracts/build/contracts/Identity.sol/Identity.json ~/workspace/pn/devlocal/contracts/Identity.json
 
-echo "mkdir -p ~/workspace/pn/visitlocal/keystore"
-mkdir -p ~/workspace/pn/visitlocal/keystore
+echo "mkdir -p ~/workspace/pn/visitlocal/contracts"
+mkdir -p ~/workspace/pn/visitlocal/contracts
 
 echo "cp resources/blockchain-test-key2.json ~/workspace/pn/visitlocal/keystore/key.json"
 cp resources/blockchain-test-key2.json ~/workspace/pn/visitlocal/keystore/key.json
@@ -71,13 +91,13 @@ echo "cp resources/arweave-test-key2.json ~/workspace/pn/visitlocal/keystore/arw
 cp resources/arweave-test-key2.json ~/workspace/pn/visitlocal/keystore/arweave.json
 echo 'Creating token in ~/workspace/pn/visitlocal/keystore/token.txt'
 echo 'xGrqHMNXLhjEubp1soD0hHN6nXIBsUwA' > ~/workspace/pn/visitlocal/keystore/token.txt
-echo "cp hardhat/build/contracts/Identity.sol/Identity.json ~/workspace/pn/visitlocal/contracts/Identity.json"
-cp hardhat/build/contracts/Identity.sol/Identity.json ~/workspace/pn/visitlocal/contracts/Identity.json
+echo "cp ../point-contracts/build/contracts/Identity.sol/Identity.json ~/workspace/pn/visitlocal/contracts/Identity.json"
+cp ../point-contracts/build/contracts/Identity.sol/Identity.json ~/workspace/pn/visitlocal/contracts/Identity.json
 
 #installing local node
 echo "installing local node"
-echo "npm i"
-npm i
+# echo "npm install --target_platform=$PLATFORM"
+# npm install --target_platform=$PLATFORM
 echo "npm run build"
 npm run build
 echo "npm run build:explorer"
