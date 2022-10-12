52fc8d4ba76c3b9a972ac1a9328e5774c846efdc
German Viescas <german@pointlabs.org>
2022-10-12 16:59:11 -0300

    Add gas estimate to Txs

--- src/rpc/decode.ts
+++ src/rpc/decode.ts
@@ -1,4 +1,5 @@
 const abiDecoder = require('abi-decoder');
+import {BigNumber} from 'ethers';
 import ethereum from '../network/providers/ethereum';
 import logger from '../core/log';
 import {CacheFactory} from '../util';
@@ -11,14 +12,23 @@ type Param = {
     type: string;
 };
 
-type DecodedTxInput = {
+export type DecodedTxInput = {
     name: string;
     params: Param[];
+    gas: {
+        value: string;
+        currency: string;
+    };
+};
+
+type ContractData = {
+    abi: object;
+    address: string;
 };
 
 // Keep track of which ABIs have been added, to avoid duplicates, with a TTL.
 const expiration = 10 * 60 * 1_000; // 10 minutes.
-const abiTracker = new CacheFactory<string, number>(expiration);
+const abiTracker = new CacheFactory<string, ContractData>(expiration);
 
 /**
  * Decode Tx and return human-readable input data.
@@ -41,20 +51,28 @@ export async function decodeTxInputData(
         return null;
     }
 
+    let contractInstance;
     const cacheKey = `${target}:${contract}`;
     if (!abiTracker.has(cacheKey)) {
         try {
             const identity = new URL(target).host.replace(/.point$/, '');
-            const {_jsonInterface} = await ethereum.loadWebsiteContract(identity, contract);
-            abiDecoder.addABI(_jsonInterface);
-            abiTracker.add(cacheKey, 1);
+            contractInstance = await ethereum.loadWebsiteContract(identity, contract);
+            abiDecoder.addABI(contractInstance._jsonInterface);
+            abiTracker.add(cacheKey, {
+                abi: contractInstance._jsonInterface,
+                address: contractInstance.options.address
+            });
         } catch (err) {
             log.error({target, contract}, 'Error fetching contract ABI.');
             return null;
         }
+    } else {
+        const {abi, address} = abiTracker.get(cacheKey)!;
+        contractInstance = ethereum.getContractFromAbi(abi, address);
     }
 
-    const decoded = abiDecoder.decodeMethod(txInputData);
+    const decoded: Omit<DecodedTxInput, 'gas'> | undefined = abiDecoder.decodeMethod(txInputData);
+
     if (!decoded) {
         log.error(
             {target, contract, txInputData},
@@ -63,5 +81,24 @@ export async function decodeTxInputData(
         return null;
     }
 
-    return decoded as DecodedTxInput;
+    const gas = {value: '', currency: ''};
+    try {
+        const method = contractInstance.methods && contractInstance.methods[decoded.name];
+        if (!method || typeof method !== 'function') {
+            throw new Error(`Method ${decoded.name} not found in contract ABI.`);
+        }
+
+        const args = decoded.params.map(p => p.value);
+        const from = ethereum.getOwner();
+        const gasAmount = await method(...args).estimateGas({from});
+        const gasPrice = await ethereum.getGasPrice();
+        gas.value = BigNumber.from(gasAmount)
+            .mul(gasPrice)
+            .toString();
+        gas.currency = 'POINT';
+    } catch (err) {
+        log.warn(err, `Unable to estimate gas for target: ${target}, contract: ${contract}.`);
+    }
+
+    return {...decoded, gas};
 }
