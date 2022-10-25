1011aa67afff2e6ffdbc7a6bcbc6fa11edb60bde
Juan Pablo Mejia <juanpmd@hotmail.com>
2022-10-25 08:10:50 -0500

    add new wallet design

--- internal/explorer.point/src/components/wallet/TokenRow.jsx
+++ internal/explorer.point/src/components/wallet/TokenRow.jsx
@@ -1,18 +1,47 @@
-const TokenRow = ({ token, network, openSendModal }) => {
-    const decimals =
-        token.decimals || token.decimals === 0 ? token.decimals : 18;
+import pointLogo from '../../assets/pointlogo.png';
+import usdcIcon from '../../assets/usdc.svg';
+import usdtIcon from '../../assets/usdt.svg';
+import daiIcon from '../../assets/dai.svg';
+
+const TOKEN_ICONS = {
+    'USDC': usdcIcon,
+    'USDT': usdtIcon,
+    'DAI': daiIcon,
+};
+
+const TOKEN_NAMES = {
+    'USDC': "USD Coin",
+    'USDT': "Tether",
+    'DAI': "Dai",
+};
+
+export default function TokenRow({ token, network, openSendModal }) {
+    const decimals = token.decimals || token.decimals === 0 ? token.decimals : 18;
 
     return (
-        <tr key={token.address}>
-            <td>
-                <strong>{token.name}</strong>
+        <tr key={token.address} className="wallet-row">
+            <td className="wallet-information">
+                <div className="icon">
+                    <img
+                        alt={token.name}
+                        src={TOKEN_ICONS[token.name]}
+                        onError={({ currentTarget }) => {
+                            currentTarget.src = pointLogo;
+                        }}
+                    />
+                </div>
+                <div className="data">
+                    <strong>{TOKEN_NAMES[token.name]}</strong>
+                    <span>{token.name}</span>
+                </div>
             </td>
-            <td className="mono">{token.address}</td>
-            <td style={{ textAlign: 'right' }}>{token.balance}</td>
-            <td style={{ textAlign: 'right' }}>
+
+            <td className="wallet-address align-middle">{token.address}</td>
+            <td className="align-middle" style={{ textAlign: 'right' }}>{token.balance}</td>
+            <td className="wallet-actions align-middle">
                 <a
                     href="#"
-                    className="btn btn-sm btn-warning"
+                    className="btn btn-sm btn-send"
                     onClick={() =>
                         openSendModal({
                             networkType: 'eth',
@@ -28,5 +57,3 @@ const TokenRow = ({ token, network, openSendModal }) => {
         </tr>
     );
 };
-
-export default TokenRow;
